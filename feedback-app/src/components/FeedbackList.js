71ad94a567e7bd4d48462a5462145943c01e3875
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-29 11:14:49 +0200

    Add animation on feedback list

--- feedback-app/src/components/FeedbackList.js
+++ feedback-app/src/components/FeedbackList.js
@@ -1,15 +1,24 @@
 import { FeedBackItem } from "./FeedbackItem";
+import { motion, AnimatePresence } from "framer-motion";
 
-
-export const FeedbackList = ({feedback, closeHandler}) => {
-    if(!feedback || feedback.length == 0 ){
-       return <p>No Feedback Yet</p>
-    }
+export const FeedbackList = ({ feedback, closeHandler }) => {
+  if (!feedback || feedback.length == 0) {
+    return <p>No Feedback Yet</p>;
+  }
   return (
     <div className="feedback-list">
-        {feedback.map((x)=> (
-           <FeedBackItem key={x.id} x={x} closeHandler={closeHandler}></FeedBackItem>
+      <AnimatePresence>
+        {feedback.map((x) => (
+          <motion.div
+            key={x.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedBackItem key={x.id} x={x} closeHandler={closeHandler} />
+          </motion.div>
         ))}
+      </AnimatePresence>
     </div>
-  )
+  );
 };
