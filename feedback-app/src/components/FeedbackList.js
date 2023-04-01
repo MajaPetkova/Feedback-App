c8d4a43a2bbea24de88a44d69efee7ca69693d0a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-01 11:34:40 +0200

    Added spinner when data loading

--- feedback-app/src/components/FeedbackList.js
+++ feedback-app/src/components/FeedbackList.js
@@ -2,31 +2,30 @@ import { FeedBackItem } from "./FeedbackItem";
 import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import { Spinner } from "./shared/Spinner";
 
 
 export const FeedbackList = () => {
 
-  const {feedback} = useContext(FeedbackContext);
+  const {feedback, isLoading} = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
-  return (
-    <div className="feedback-list">
-      <AnimatePresence>
-        {feedback.map((x) => (
-          <motion.div
-            key={x.id}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <FeedBackItem key={x.id} x={x} 
-            // closeHandler={closeHandler} 
-            />
-          </motion.div>
-        ))}
-      </AnimatePresence>
-    </div>
-  );
-};
+  return isLoading ? <Spinner/> : (<div className="feedback-list">
+  <AnimatePresence>
+    {feedback.map((x) => (
+      <motion.div
+        key={x.id}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <FeedBackItem key={x.id} x={x} 
+        // closeHandler={closeHandler} 
+        />
+      </motion.div>
+    ))}
+  </AnimatePresence>
+</div>);
+ };
