2d619fb86857ce73ed27456451705f203b79f650
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-05 13:00:22 +0200

    Add useReducer hook

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -1,20 +1,37 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import { GithubReducer } from "./GithubReducer";
 
-export const GithubContext= createContext();
+export const GithubContext = createContext();
 // const url= "https://api.github.com/users";
 
-export const GithubProvider = ({children}) =>{
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+export const GithubProvider = ({ children }) => {
+  //   const [users, setUsers] = useState([]);
+  //   const [loading, setLoading] = useState(true);
 
-    const fetchUsers= async() =>{
-        const res= await fetch("https://api.github.com/users")
-        const data=await res.json();
-       setUsers(data)
-       setLoading(false)
-     }
-  
-     return <GithubContext.Provider value ={{users, loading , fetchUsers} }>
-        {children}
-     </GithubContext.Provider>
-}
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  const fetchUsers = async () => {
+    const res = await fetch("https://api.github.com/users");
+    const data = await res.json();
+    // setUsers(data);
+    // setLoading(false);
+    dispatch({
+        type :"GET_USERS",
+        payload :data
+    })
+  };
+
+  return (
+    <GithubContext.Provider value={{ 
+        users: state.users,
+        loading: state.loading, 
+        fetchUsers }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
