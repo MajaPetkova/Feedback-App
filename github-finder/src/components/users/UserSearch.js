51f9c3cede0a25111ccfd3f8d743eee2b947a5e6
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-05 15:16:10 +0200

    Update footer.css and make form controlled

--- github-finder/src/components/users/UserSearch.js
+++ github-finder/src/components/users/UserSearch.js
@@ -1,16 +1,37 @@
+import { useState, useContext } from "react";
+import { GithubContext } from "../../context/github/GithubContext";
 
 export const UserSearch = () => {
-   return(
+  const [text, setText] = useState("");
+  const { users } = useContext(GithubContext);
+
+  const handleChange = (ev) => {
+    setText(ev.target.value);
+  };
+  const submitHandler = (ev) => {
+    ev.preventDefault();
+    if (text === "") {
+      alert("Please enter something");
+    } else {
+      // todo search users
+      setText("");
+    }
+  };
+  return (
     <div className="search">
-        <div className="form">
-            <form className="form-control">
-             <input placeholder="Search"/>
-             <button className="btn-search">Go</button>
-            </form>
-        </div>
-        <button className="btn-clear">
-            Clear
-        </button>
+      <div className="form">
+        <form onSubmit={submitHandler} className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={handleChange}
+            value={text}
+          />
+          <button className="btn-search">Go</button>
+        </form>
+      </div>
+      {users.length >0 && <button className="btn-clear">Clear</button>}
+      
     </div>
-   ) 
+  );
 };
