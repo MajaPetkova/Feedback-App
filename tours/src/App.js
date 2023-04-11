8dc9c43bf045cf0ded340ee82ad2a29a7e9b3c6a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-11 19:30:55 +0200

    Added components, remove strictMode, and fetching data

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Loading } from "./Loading";
 
 function App() {
+  const url = "https://course-api.com/react-tours-project";
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setTours(tours);
+      setIsLoading(false)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchTours();
+  }, []);
+
+  if(isLoading){
+    return (<Loading/>)
+  }
   return (
-    <div className="App">
-    <h1>Tours Starter</h1>
-    </div>
+    <main className="App">
+      <h1>Tours Starter</h1>
+    </main>
   );
 }
 
---
+++ tours/src/Loading.js
@@ -0,0 +1,7 @@
+import loadingSpinner from "../src/components/assets/loading.gif"
+export const Loading =() =>{
+    return(<div className="loading">
+        <img src={loadingSpinner} alt="loading"/>
+    </div>
+    )
+}
