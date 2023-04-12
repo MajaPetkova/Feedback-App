0df168e47a41b840de28d954211eff15d971599a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-12 20:18:32 +0200

    Add state form read more, toggle on button

--- tours/src/Tour.js
+++ tours/src/Tour.js
@@ -1,11 +1,16 @@
+import{useState} from "react";
+
 export const Tour = ({ tours, removeTour }) => {
+    const[readMore, setReadMore] = useState(false);
+    // console.log(tours.info.substring(0, 10))
   return (
     <article className="single-tour">
       <img src={tours.image} alt="logo" className="img"/>
       <span className="tour-price">$ {tours.price}</span>
       <div className="info-tour">
         <h5>{tours.name}</h5>
-        <p>{tours.info}</p>
+        <p>{readMore ? tours.info : `${tours.info.substring(0, 100)}...`}</p>
+        <button className="info-btn" onClick={()=> setReadMore(!readMore)}>{readMore ? "show less": "read more"}</button>
         <button type="button" className="btn" onClick={()=>removeTour(tours.id)}>not interested</button>
       </div>
     </article>
