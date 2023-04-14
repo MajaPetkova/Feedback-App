c1f2a45c06da4c727e23b55fb1475493bce9bdd0
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-14 17:20:19 +0200

    Added state for the index and css

--- reviews-starter/src/App.js
+++ reviews-starter/src/App.js
@@ -1,11 +1,27 @@
-
-import './App.css';
+import { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  console.log(name);
   return (
-    <div className="App">
-    
-    </div>
+    <main>
+      <article className="card">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img"/>
+          <span><FaQuoteRight/></span>
+        </div>
+        <h4>{name}</h4>
+        <p>{job}</p>
+        <p>{text}</p>
+        <div className="buttons">
+          <FaChevronLeft/>
+          <FaChevronRight/>
+        </div>
+      </article>
+    </main>
   );
 }
 
