e592f7e5e594c5c03e634eb3b770edc564310d07
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-17 12:19:39 +0200

    Add button for choosing random person and css

--- reviews-starter/src/App.js
+++ reviews-starter/src/App.js
@@ -5,40 +5,57 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 function App() {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
- 
-  const nextPerson =() =>{
-    setIndex((currentIndex)=>{
-       const newIndex= currentIndex + 1;
-       if(newIndex > people.length-1){
-        return 0
-      }
-       return newIndex;
-    })
-  }
-  const prevPerson =() =>{
-    setIndex((currentIndex)=>{
-      const newIndex= currentIndex - 1;
-      if(newIndex < 0){
-        return people.length-1
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      if (newIndex > people.length - 1) {
+        return 0;
       }
       return newIndex;
-   })
-  }
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      if (newIndex < 0) {
+        return people.length - 1;
+      }
+      return newIndex;
+    });
+  };
+  const randomPerson = () => {
+    let random = Math.floor(Math.random() * people.length);
+    if (random == index){
+      random = index + 1;
+    }
+    setIndex(random)
+  };
   return (
     <main>
       <article className="card">
         <div className="img-container">
-          <img src={image} alt={name} className="person-img"/>
-          <span><FaQuoteRight/></span>
+          <img src={image} alt={name} className="person-img" />
+          <span>
+            <FaQuoteRight />
+          </span>
         </div>
         <h4 className="name">{name}</h4>
         <p className="job">{job}</p>
         <p className="text">{text}</p>
-        
+
         <div className="buttons">
-          <button className="prev-btn" onClick={prevPerson}><FaChevronLeft/></button>
-          <button className="next-btn" onClick={nextPerson}> <FaChevronRight/></button>
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            {" "}
+            <FaChevronRight />
+          </button>
         </div>
+        <button className="btn" onClick={randomPerson}>
+          Surprise me
+        </button>
       </article>
     </main>
   );
