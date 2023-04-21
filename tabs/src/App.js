bb3aa6ed52eac3927525177a3002ee1eaf261742
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-21 14:53:15 +0200

    fetching data and rendering Loading before data render

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,11 +1,30 @@
-
+import { useState, useEffect } from "react";
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  return (
-    <div className="App">
- 
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setJobs(data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading">
+          <h1>Loading...</h1>
+        </div>
+      </section>
+    );
+  }
+
+  return <main className="container"></main>;
 }
 
 export default App;
