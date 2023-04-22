0e2c612c0e3f391e8973c5616588ff52832ac0fd
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-22 20:26:25 +0200

    Add button and submit handler

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -4,17 +4,37 @@ import data from "./data";
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
+ 
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
 
+    setText(data.slice(0, amount))
+  };
 
   return (
     <section className="container">
       <h4>Tired of boring Lorem Ipsum ?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs:</label>
-        <input type="number" name="amount" id="amount" min="1" step="1" max="8" value={count} onChange={(e)=>setCount(e.target.value)} />
-        <button type="submit" className="btn">Generate</button>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
       </form>
+    <article className="lorem-text"> 
+     {text.map((x, index) => {return <p key={index}>{x}</p>})}
+    </article>
     </section>
   );
 }
