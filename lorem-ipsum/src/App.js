3239cae44ba3f6a922a339ea2ae3b3af78d4be84
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-22 20:01:05 +0200

    Add css, and section in app.js

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import data from "./data";
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+
+
   return (
-    <div className="App">
-    
-    </div>
+    <section className="container">
+      <h4>Tired of boring Lorem Ipsum ?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">Paragraphs:</label>
+        <input type="number" name="amount" id="amount" min="1" step="1" max="8" value={count} onChange={(e)=>setCount(e.target.value)} />
+        <button type="submit" className="btn">Generate</button>
+      </form>
+    </section>
   );
 }
 
