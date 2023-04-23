7739a9c01eaa4561fcc426893c19598708e69512
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-23 21:21:23 +0200

    Add nanoid library and add-Item functionality

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -1,13 +1,24 @@
-import{useState} from "react"
+import { useState } from "react";
 import { Form } from "./components/Form";
-
+import { nanoid } from "nanoid";
 
 function App() {
   const [items, setItems] = useState([]);
-  
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    setItems([...items, newItem])
+    console.log(newItem)
+  };
+
+
   return (
     <section className="container">
-     <Form/>
+      <Form addItem={addItem}/>
     </section>
   );
 }
--- grocery-bud/src/components/Form.js
+++ grocery-bud/src/components/Form.js
@@ -1,19 +1,31 @@
-import{useState} from "react"
+import { useState } from "react";
 
-export const Form = () => {
-    const[newItemName, setNewItemName] = useState("");
+export const Form = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState("");
 
-    const handleSubmit =(e) =>{
-     e.preventDefault();
-     console.log(newItemName)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItemName) {
+      return;
+    } else {
+      addItem(newItemName);
+      setNewItemName("");
     }
-    return(
-        <div className="form-block">
-            <h4>Grocery Bud</h4>
-            <form  onSubmit={handleSubmit}>
-            <input type="text" className="input-text" value={newItemName} onChange={(e) =>setNewItemName(e.target.value)} />
-            <button className="btn" type="submit">Add Item</button>
-            </form>
-        </div>
-    )
+  };
+  return (
+    <div className="form-block">
+      <h4>Grocery Bud</h4>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input-text"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Add Item
+        </button>
+      </form>
+    </div>
+  );
 };
