e919dbe3c16354cf912808413e8e01c208db06ee
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-23 18:14:15 +0200

    Add newColor function for setting colours

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -4,12 +4,20 @@ import { Form } from "./components/Form";
 import Values from "values.js";
 
 function App() {
-  const [colors, setColors] = useState(new Values("#f15025").all(10));
+  const [colors, setColors] = useState([]);
   // console.log(new Values("#f15025").all(10))
 
+  const addColor =(color) =>{
+    try{
+    const newColors=  new Values(color).all(10);
+    setColors(newColors)
+    }catch(err){
+      alert(err.message)
+    }
+  }
   return (
     <main>
-      <Form />
+      <Form addColor={addColor}/>
       <ColorList colors={colors}/>
     </main>
   );
