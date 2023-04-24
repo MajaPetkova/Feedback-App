06d57d2adc3df46dff016c1791611af3eb04f1f8
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-24 10:09:08 +0200

    Set up local storage functionality for data persisting

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -3,8 +3,24 @@ import { Form } from "./components/Form";
 import { nanoid } from "nanoid";
 import { ItemList } from "./components/ItemList";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+  return list;
+  // console.log(list)
+};
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
+
+
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -12,18 +28,20 @@ function App() {
       completed: false,
       id: nanoid(),
     };
-    setItems([...items, newItem])
-    console.log(newItem)
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
-const removeItem=(itemId)=>{
-  const newItemsArray= items.filter(x =>x.id !== itemId);
-  setItems(newItemsArray)
-}
+  const removeItem = (itemId) => {
+    const newItemsArray = items.filter((x) => x.id !== itemId);
+    setItems(newItemsArray);
+    setLocalStorage(newItemsArray);
+  };
 
   return (
     <section className="container">
-      <Form addItem={addItem}/>
-      <ItemList items={items} removeItem={removeItem}/>
+      <Form addItem={addItem} />
+      <ItemList items={items} removeItem={removeItem} />
     </section>
   );
 }
