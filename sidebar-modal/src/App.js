95d4b85f1f38fa4d144530470129ff517755911e
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-25 21:34:03 +0200

    Initialize new project and add components

---
+++ sidebar-modal/src/App.js
@@ -0,0 +1,15 @@
+import { Modal } from "./components/Modal";
+import { Home } from "./components/Home";
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <main>
+    <Home/>
+    <Modal/>
+    <Sidebar/>
+    </main>
+  );
+}
+
+export default App;
