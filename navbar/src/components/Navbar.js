7848835265b47f62fc5f0194f69c9a889d244c45
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-25 20:41:07 +0200

    Add navbar dynamic setup with UseRef

--- navbar/src/components/Navbar.js
+++ navbar/src/components/Navbar.js
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import { social, links } from "../data";
 import logo from "../logo.svg";
 
 export const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
- 
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
   const toggleLinks = () => {
+    console.log()
     setShowLinks(!showLinks);
   };
 
-
+  const linkStyles= {
+     height: showLinks ? `${linksRef.current.getBoundingClientRect().height}px` :`0px`
+  }
   return (
     <nav>
       <div className="nav-center">
@@ -21,18 +26,19 @@ export const Navbar = () => {
           </button>
         </div>
         {/* {showLinks && ( */}
-          <div className={showLinks ? "links-container show-container" :"links-container"}>
-            <ul className="links">
-              {links.map((x) => {
-                const { id, text, url } = x;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+        {/* <div className={showLinks ? "links-container show-container" :"links-container"}> */}
+        <div className="links-container" ref={linksContainerRef} style={linkStyles}>
+          <ul className="links" ref={linksRef}>
+            {links.map((x) => {
+              const { id, text, url } = x;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         {/* )} */}
       </div>
     </nav>
