60e3d33c92d1fead24a0a7667e41d6ecdd7243d3
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-26 21:19:08 +0200

    Add buttons in Home comp. add context and remove strict mode

--- sidebar-modal/src/App.js
+++ sidebar-modal/src/App.js
@@ -2,13 +2,16 @@ import { Modal } from "./components/Modal";
 import { Home } from "./components/Home";
 import { Sidebar } from "./components/Sidebar";
 
+
 function App() {
   return (
-    <main>
-    <Home/>
-    <Modal/>
-    <Sidebar/>
-    </main>
+   
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+
   );
 }
 
--- sidebar-modal/src/components/Home.js
+++ sidebar-modal/src/components/Home.js
@@ -1,3 +1,18 @@
+import { useContext } from "react";
+import { AppContext } from "../context/context";
+import { FaBars } from "react-icons/fa";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const { openSidebar, openModal } = useContext(AppContext);
+  // console.log(openModal)
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        show modal
+      </button>
+    </main>
+  );
 };
