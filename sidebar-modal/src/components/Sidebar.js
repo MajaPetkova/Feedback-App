0e859a1444377f9faf6b43a054730722d7481977
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-26 22:34:52 +0200

    Add logo.svg and sidebar elements

--- sidebar-modal/src/components/Sidebar.js
+++ sidebar-modal/src/components/Sidebar.js
@@ -1,3 +1,20 @@
+import { useContext } from "react";
+import { AppContext } from "../context/context";
+import logo from "../logo.svg";
+import { social, links } from "../data";
+import { FaTimes } from "react-icons/fa";
+
 export const Sidebar = () => {
-    return <div>Sidebar</div>;
-  };
+  const { isSidebarOpen, closeSidebar } = useContext(AppContext);
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo"/>
+        <button className="btn-side" onClick={closeSidebar}><FaTimes/></button>
+      </div>
+      <ul className="social-links">
+        {links.map(x => {return <li key={x.id}><a href={x.url}>{x.icon}{x.text}</a></li>})}
+      </ul>
+    </aside>
+  );
+};
