25447cd4d14afd3fe7d034968d4cb6a67d743a07
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-26 23:19:42 +0200

    Add css for sidebar and logic for open - close sidebar

--- sidebar-modal/src/components/Sidebar.js
+++ sidebar-modal/src/components/Sidebar.js
@@ -7,13 +7,16 @@ import { FaTimes } from "react-icons/fa";
 export const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useContext(AppContext);
   return (
-    <aside className="sidebar">
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
       <div className="sidebar-header">
         <img src={logo} alt="logo" className="logo"/>
         <button className="btn-side" onClick={closeSidebar}><FaTimes/></button>
       </div>
       <ul className="social-links">
-        {links.map(x => {return <li key={x.id}><a href={x.url}>{x.icon}{x.text}</a></li>})}
+        {links.map(x => {return <li key={x.id}><a href={x.url}>{x.icon} {x.text}</a></li>})}
+      </ul>
+      <ul className="icon-links">
+        {social.map(x => {return <li key={x.id}><a href={x.url}>{x.icon}</a></li>})}
       </ul>
     </aside>
   );
