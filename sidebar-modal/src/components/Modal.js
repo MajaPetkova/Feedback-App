b7284bcd2a7608f4707da3b9857b67ad8cbbf90f
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-26 22:04:42 +0200

    Add css for modal component and logic for close modal

--- sidebar-modal/src/components/Modal.js
+++ sidebar-modal/src/components/Modal.js
@@ -1,3 +1,15 @@
+import { useContext } from "react";
+import { AppContext } from "../context/context";
+import { FaTimes } from "react-icons/fa";
+
 export const Modal = () => {
-    return <div>Modal</div>;
-  };
+  const { isModalOpen, closeModal } = useContext(AppContext);
+
+
+  return <div className={isModalOpen ? "modal-overlay show-modal": "modal-overlay"}>
+   <div className="modal-container">
+    <h3>Modal Content</h3>
+    <button className="btn-close" onClick={closeModal}><FaTimes/></button>
+   </div>
+  </div>;
+};
