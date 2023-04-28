29158cd362499faa0be829c61e8f7d9ae928bc5a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-28 23:03:38 +0200

    Add css for submenu

--- strapi/src/components/Submenu.js
+++ strapi/src/components/Submenu.js
@@ -8,7 +8,7 @@ const currentPage= sublinks.find(x => x.pageId === pageId);
 
 
     return(
-        <div className="submenu">
+        <div className={currentPage ? "submenu show-submenu" : "submenu" }>
          <h5>{currentPage?.page  }</h5>
          <div className="submenu-links" style={{gridTemplateColumns:currentPage?.links?.length >3 ? "1fr 1fr" : "1fr"  }}>
           {currentPage?.links?.map(x =>{
