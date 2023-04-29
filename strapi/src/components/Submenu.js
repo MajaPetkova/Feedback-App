77a20c93c5b123b9b3d5857a317a6b0f1d81a5c4
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-29 10:22:00 +0200

    Update css and update hiding menu using useRef and onMouseLeave event

--- strapi/src/components/Submenu.js
+++ strapi/src/components/Submenu.js
@@ -1,14 +1,25 @@
 import { AppContext } from "../context/Context";
-import {useContext} from "react";
-import sublinks from "../data"
+import {useContext, useRef} from "react";
+import sublinks from "../data";
 
 export const Submenu=()=>{
-const {pageId} = useContext(AppContext)
+const {pageId, setPageId} = useContext(AppContext)
 const currentPage= sublinks.find(x => x.pageId === pageId);
+const  submenuContainer = useRef(null);
+
+const handleMouse=(event)=>{
+    const submenu =  submenuContainer.current;
+    const {left, right, bottom} = submenu.getBoundingClientRect();
+    const{clientX, clientY} = event;
+    // console.log(result)
+    if(clientX < left -1 || clientX > right-1 || clientY > bottom -1) {
+        setPageId(null)
 
 
+    }
+}
     return(
-        <div className={currentPage ? "submenu show-submenu" : "submenu" }>
+        <div className={currentPage ? "submenu show-submenu" : "submenu" } onMouseLeave={handleMouse} ref={submenuContainer}>
          <h5>{currentPage?.page  }</h5>
          <div className="submenu-links" style={{gridTemplateColumns:currentPage?.links?.length >3 ? "1fr 1fr" : "1fr"  }}>
           {currentPage?.links?.map(x =>{
