a4713f9d8a4f07b1895a1367a6fc0def25d0f554
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-29 19:37:14 +0200

    Update CardComponent if no Items,  update css, update item component

--- card/src/components/CartContainer.js
+++ card/src/components/CartContainer.js
@@ -1,26 +1,46 @@
-import data from "../data";
+import { AppContext } from "../context/context";
+import cartItems from "../data";
 import { CartItem } from "./CartItem";
+import { useContext } from "react";
 
-export const CartContainer=() =>{
-    return(
-<section className="cart">
-    <header>
+export const CartContainer = () => {
+    
+    const {cart} = useContext(AppContext)
+    const cartArray = Array.from (cart.entries())
+    // console.log(cartArray)
+
+    if(cartItems.length === 0){
+        return (
+            <section className="cart">
+             <header>
+                <h2>Your Bag</h2>
+                <h4 className="empty-cart">is currently empty</h4>
+             </header>
+            </section>
+        )
+    }
+  return (
+    <section className="cart">
+      <header>
         <h2>Your bag</h2>
-    </header>
-    <div>
-     {data.map(x =>{
-        return <CartItem key={x.id} {...x}/>
-     })}
-    </div>
-    <footer>
+      </header>
+      <div>
+        {cartArray.map((x) => {
+            const[id, item]= x;
+          return <CartItem key={id} {...item} />;
+        })}
+      </div>
+      <footer>
         <hr />
         <div className="cart-total">
-            <h4>
-                total <span>$0.00</span>
-            </h4>
+          <h4>
+            total <span>$0.00</span>
+          </h4>
         </div>
-        <button className="btn clear-btn" onClick={console.log("clear all")}>Clear Cart</button>
-    </footer>
-</section>
-    )
-}
+        <button className="btn clear-btn" onClick={console.log("clear all")}>
+          Clear Cart
+        </button>
+      </footer>
+    </section>
+  );
+};
--- card/src/components/CartItem.js
+++ card/src/components/CartItem.js
@@ -1,6 +1,9 @@
-import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+import {useContext} from "react"
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+import { AppContext } from "../context/context";
 
 export const CartItem = ({ ...x }) => {
+
   return (
     <article className="cart-item">
       <div className="details">
@@ -13,11 +16,11 @@ export const CartItem = ({ ...x }) => {
       </div>
       <div className="amount-btn">
         <button className="btn-arrow" onClick={()=>console.log("increase")}>
-          <FaAngleUp />
+          <FaChevronUp />
         </button>
         <p className="amount">{x.amount}</p>
         <button className="btn-arrow" onClick={()=>console.log("decrease")}>
-          <FaAngleDown />
+          <FaChevronDown />
         </button>
       </div>
     </article>
