c41f006a5b75f5b52ed0e4bc93bc9f65947d0855
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-02 17:51:34 +0200

    Update controlled inputs with user list and added users

--- demo/src/components/forms/ControlledInputs.jsx
+++ demo/src/components/forms/ControlledInputs.jsx
@@ -3,12 +3,21 @@ import { useState } from "react";
 export const ControlledInputs = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]) ;
 
  const submitHandler =(e)=>{
    e.preventDefault();
-   console.log(name, email)
+   if(name == "" || email == ""){
+    return
+   }
+  const fakeId= Date.now();
+  const newUser = {id:fakeId, name, email};
+  const updatedUsers= [...users, newUser]
+ setUsers(updatedUsers);
+ setName("");
+ setEmail("")
  }
-  return (
+  return (<>
     <form className="form" onSubmit={submitHandler}>
       <h4>Controlled Inputs</h4>
       <div className="form-row">
@@ -21,7 +30,7 @@ export const ControlledInputs = () => {
           id="name"
           value={name}
           onChange={(e)=>setName(e.target.value)}
-        />
+          />
       </div>
       <div className="form-row">
         <label htmlFor="email" className="form-label">
@@ -33,11 +42,18 @@ export const ControlledInputs = () => {
           id="email"
           value={email}
           onChange={(e)=>setEmail(e.target.value)}
-        />
+          />
       </div>
       <button type="submit" className="btn">
         Submit
       </button>
     </form>
+    <div>
+        <ul>
+        {users.map(x=> {return <li key={x.id}>{x.name} {x.email}</li>})}
+        </ul>
+    </div>
+          </>
+    
   );
 };
