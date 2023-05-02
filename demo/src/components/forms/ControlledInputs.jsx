b5c968fd4b120a4e60effe32e211f6fe4b67a400
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-02 20:50:23 +0200

    Add delete for user

--- demo/src/components/forms/ControlledInputs.jsx
+++ demo/src/components/forms/ControlledInputs.jsx
@@ -3,57 +3,72 @@ import { useState } from "react";
 export const ControlledInputs = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [users, setUsers] = useState([]) ;
+  const [users, setUsers] = useState([]);
 
- const submitHandler =(e)=>{
-   e.preventDefault();
-   if(name == "" || email == ""){
-    return
-   }
-  const fakeId= Date.now();
-  const newUser = {id:fakeId, name, email};
-  const updatedUsers= [...users, newUser]
- setUsers(updatedUsers);
- setName("");
- setEmail("")
- }
-  return (<>
-    <form className="form" onSubmit={submitHandler}>
-      <h4>Controlled Inputs</h4>
-      <div className="form-row">
-        <label htmlFor="name" className="form-label">
-          name
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={(e)=>setName(e.target.value)}
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (name == "" || email == "") {
+      return;
+    }
+    const fakeId = Date.now();
+    const newUser = { id: fakeId, name, email };
+    const updatedUsers = [...users, newUser];
+    setUsers(updatedUsers);
+    setName("");
+    setEmail("");
+  };
+
+  const deleteItem =(id)=>{
+    const newArr = users.filter(x=> x.id !== id)
+    setUsers(newArr);
+ 
+  }
+  return (
+    <>
+      <form className="form" onSubmit={submitHandler}>
+        <h4>Controlled Inputs</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
-      </div>
-      <div className="form-row">
-        <label htmlFor="email" className="form-label">
-          email
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-      </div>
-      <button type="submit" className="btn">
-        Submit
-      </button>
-    </form>
-    <div>
+        </div>
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+      <div>
         <ul>
-        {users.map(x=> {return <li key={x.id}>{x.name} {x.email}</li>})}
+          {users.map((x) => {
+            return (
+              <div key={x.id}>
+                <li>
+                  {x.name} {x.email}
+                </li>
+                <button onClick={()=>deleteItem(x.id)}>Delete</button>
+              </div>
+            );
+          })}
         </ul>
-    </div>
-          </>
-    
+      </div>
+    </>
   );
 };
