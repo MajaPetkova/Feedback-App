86b98b14bf5cc92297e5ecad7e86d7a079fad001
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-03 09:50:44 +0200

    Add checkbox and select input

--- demo/src/components/forms/MultipleInputs.jsx
+++ demo/src/components/forms/MultipleInputs.jsx
@@ -1,65 +1,97 @@
-import { useState } from "react"
-
-export const MultipleInputs= () =>{
-    const [user, setUser] = useState({
-        name: "",
-        email:"",
-        password:""
-    })
-   const handleChange =(e)=>{
-      setUser({...user,[e.target.name]: e.target.value})
-     
-   }
+import { useState } from "react";
+const frameworks= ["react", "vue", "angular", "svelte"];
 
 
-   const submitHandler =(e) =>{
-   e.preventDefault();
-   console.log(user)
-   }
+export const MultipleInputs = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState("react");
 
+  const handleShipping = (e) => {
+    setShipping(e.target.checked);
+  };
+  
+  const handleFramework = (e) => {
+setFramework(e.target.value)
+  };
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-    return(
-        <form className="form" onSubmit={submitHandler}>
-        <h4>Controlled Inputs</h4>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+    console.log(shipping)
+  };
+
+  return (
+    <form className="form" onSubmit={submitHandler}>
+      <h4>Controlled Inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={user.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={user.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="">
+        <label htmlFor="shipping" className="form-label">
+          Free shipping
+        </label>
+        <input
+          type="checkbox"
+          name="shipping"
+          id="shipping"
+          checked={shipping}
+          onChange={handleShipping}
+        />
+      </div>
+      <div className="form-row">
+          <label htmlFor="framework" className="form-label">
+         Framework
           </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={user.name}
-            onChange={handleChange}
-          />
+        <select name="framework" id="framework" value={framework} onChange={handleFramework}>
+          {frameworks.map(x=>
+            <option key={x}>{x}</option>
+          )}
+        </select>
         </div>
-        <div className="form-row">
-          <label htmlFor="email" className="form-label">
-            email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={user.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="email" className="form-label">
-            password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={user.password}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn">
-          Submit
-        </button>
-      </form>
-    )
-}
+      <button type="submit" className="btn">
+        Submit
+      </button>
+    </form>
+  );
+};
