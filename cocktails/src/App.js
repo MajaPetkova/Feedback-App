5b1e78348019b2fe69ab79c33ee3ca4a7f0c39c4
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-07 17:37:16 +0200

    Add search form and functionality and css

--- cocktails/src/App.js
+++ cocktails/src/App.js
@@ -12,15 +12,12 @@ function App() {
     <BrowserRouter>
       <Navbar/>
     <Routes>
-      <Route exact path="/" element={ <Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/cocktail/:id" element={<SingleCocktail/>}></Route>
-      <Route path="*" element={<Error/>}></Route>
+      <Route exact path="/" element={ <Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/cocktail/:id" element={<SingleCocktail/>}/>
+      <Route path="*" element={<Error/>}/>
     </Routes>
     </BrowserRouter>
-   
-    
-    
   </div>;
 }
 
--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -1,12 +1,36 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import { AppContext } from "../context";
 
 export const Search = () => {
   const { setSearchTerm } = useContext(AppContext);
+  const searchValue = useRef("");
+
+  useEffect(()=>{
+      searchValue.current.focus()
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit =(e) =>{
+   e.preventDefault()
+  }
 
   return (
     <section className="search">
- 
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="martini"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
     </section>
   );
 };
