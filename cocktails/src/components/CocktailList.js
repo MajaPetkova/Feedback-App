a046d0f9b2f84d9faf57d8d257cdea4626028a98
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-07 15:21:03 +0200

    Add fetching data and visualize list of cocktails , css

--- cocktails/src/components/CocktailList.js
+++ cocktails/src/components/CocktailList.js
@@ -1,21 +1,26 @@
-import { AppContext } from "../context"
-import {useContext} from "react";
+import { AppContext } from "../context";
+import { useContext } from "react";
 import { Loading } from "./Loading";
+import { CocktailItem } from "./CocktailItem";
 
-
-export const CocktailList = () =>{
-    const {loading, cocktails} = useContext(AppContext);
-
-    if(loading) {
-        return <Loading/>
-    }
-    if(cocktails.length < 1){
-       return <h2 className="no-found">no cocktails mached your search criteria</h2>
-    }
+export const CocktailList = () => {
+  const { loading, cocktails } = useContext(AppContext);
+  console.log(cocktails);
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
     return (
-        <section className="">
-         
-   
-        </section>
-    )
-}
+      <h2 className="no-found">no cocktails mached your search criteria</h2>
+    );
+  }
+  return (
+    <section className="section-list">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktail-container">{cocktails.map((x) => {
+      return <CocktailItem key={x.id} {...x} />
+      })}
+      </div>
+    </section>
+  );
+};
--- cocktails/src/context.js
+++ cocktails/src/context.js
@@ -1,20 +1,52 @@
-import {createContext, useState} from "react";
+import { createContext, useState, useEffect } from "react";
 
-const url ="http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export const AppContext = createContext();
 
-export const AppProvider =({children}) =>{
-const [loading, setLoading] = useState(true);
-const [searchTerm, setSearchTerm] = useState("a");
-const [cocktails, setCocktails] = useState([]);
+export const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
 
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try {
+      const resp = await fetch(`${url}${searchTerm}`);
+      const data = await resp.json();
+      const { drinks } = data;
+    //   console.log(drinks);
+      if (drinks) {
+        const newDrinks = drinks.map((x) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = x;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newDrinks);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm]);
 
-
-    return(
-        <AppContext.Provider value={{loading, searchTerm, cocktails, setSearchTerm}}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider
+      value={{ loading, searchTerm, cocktails, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
