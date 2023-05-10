74d22f91a79d4b021d7b6520aceb410f814bf4e6
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-10 23:11:48 +0200

    Add component and css

--- random-user/src/App.js
+++ random-user/src/App.js
@@ -1,10 +1,26 @@
+import {FaUserAlt,FaRegCalendarTimes,FaMap, FaPhone, FaLock, FaEnvelopeOpen}  from "react-icons/fa";
 
+const url = "https://randomuser.me/api/";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <main>
+      <div className="bgg"></div>
+      <div className="container">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHh9Y7tcmwEr_od64uK2G5DokOr_yxia1xVw&usqp=CAU" alt="name" />
+        <p>My name is</p>
+        <h2>Anna</h2>
+        <div className="icons">
+          <FaUserAlt/>
+          <FaEnvelopeOpen/>
+          <FaRegCalendarTimes/>
+          <FaMap/>
+          <FaPhone/>
+          <FaLock/>
+        </div>
+        <button className="btn">Random User</button>
+      </div>
+    </main>
   );
 }
 
