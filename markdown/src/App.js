382458fbd31392136d2d34733e7a6a2f985e350f
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-10 11:21:01 +0200

    add css and markdown component for the value in the input

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 
 function App() {
+  const [markdown, setMarkdown]= useState("## Markdown Preview  ![image](https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2022-10/girl-names-that-start-with-c-zz-221027-768b76.jpg) **be happy**");
+
   return (
-    <div className="App">
-   
-    </div>
+    <main className="markdown">
+      <section className="input">
+        <textarea type="input" value={markdown} onChange={(e)=> setMarkdown(e.target.value)}>
+        </textarea>
+        <article className="result">
+        <ReactMarkdown>
+         {markdown}
+        </ReactMarkdown>
+        </article>
+      </section>
+    </main>
   );
 }
 
