c8eeb8281010cc01cb5d5c093c1abda15e08f894
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-11 19:28:04 +0200

    Dynamically changing title and value

--- random-user/src/App.js
+++ random-user/src/App.js
@@ -7,25 +7,29 @@ const defaultImg= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHh9Y7t
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const[user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
   const [title, setTitle] = useState("name");
   const [value, setValue] = useState("random person");
 
   const handleValue =(e)=> {
-   value(e.target)
+    if(e.target.classList.contains("icon")){
+     const newValue= e.target.dataset.label 
+     setTitle(newValue)
+     setValue(user[newValue])
+    }
   }
 
   const userLoading = async() =>{
     const res= await fetch(url)
     const data=await res.json();
     const user= data.results[0];
-    
+
     const {email, phone } = user;
     const { large: img} = user.picture;
     const {login:{username}} = user;
     const {first, last} = user.name;
     const{dob:{age}} = user;
-   const {name, number} = user.location.street
+    const {name, number} = user.location.street;
   
 
    const person = {img, email, phone, username, name:`${first} ${last}`, age, street: `${name} ${number}`}
@@ -50,7 +54,7 @@ function App() {
          <button className="icon" data-label= "age" onMouseOver={handleValue}> <FaRegCalendarTimes/></button>
          <button className="icon" data-label= "street" onMouseOver={handleValue}> <FaMap/></button>
          <button className="icon" data-label= "phone" onMouseOver={handleValue}>  <FaPhone/></button>
-         <button className="icon" data-label= "username"  onMouseOver={handleValue}>  <FaLock/></button>
+         <button className="icon" data-label= "username" onMouseOver={handleValue} >  <FaLock/></button>
         </div>
         <button className="btn"  onClick={userLoading}>{loading ? "loading..." : "Random User"}</button>
       </div>
