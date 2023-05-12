463b0f31571eaff0dcb1700b7a896465754333d5
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-12 19:06:46 +0200

    Add css, image default and html

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,10 +1,49 @@
-
-
+import stock from "./stock.jpg"
 function App() {
   return (
-    <div className="App">
-   
+    <main>
+    <div className="app">
+      <h2>Pagination </h2>
+      <div className="underline"></div>
     </div>
+    <section className="followers">
+      <div className="container">
+        <article className="card">
+          <img src={stock} alt="" className="img"/>
+          <h4>name</h4>
+          <button> view profile</button>
+        </article>
+        <article className="card">
+          <img src={stock} alt="" className="img"/>
+          <h4>name</h4>
+          <button className="btn"> view profile</button>
+        </article>
+        <article className="card">
+          <img src={stock} alt="" className="img"/>
+          <h4>name</h4>
+          <button> view profile</button>
+        </article>
+        <article className="card">
+          <img src={stock} alt="" className="img"/>
+          <h4>name</h4>
+          <button className="btn"> view profile</button>
+        </article>
+        <article className="card">
+          <img src={stock} alt="" className="img"/>
+          <h4>name</h4>
+          <button> view profile</button>
+        </article>
+        <article className="card">
+          <img src={stock} alt="" className="img"/>
+          <h4>name</h4>
+          <button className="btn"> view profile</button>
+        </article>
+      </div>
+       <div className="btn-container">
+        <button className="pgn">1</button>
+       </div>
+    </section>
+    </main>
   );
 }
 
