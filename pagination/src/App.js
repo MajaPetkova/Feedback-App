b9c8b1b0313503e7297608235bd73851882e41c3
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-13 11:38:32 +0200

    Added buttons for the pages and visualize buttons

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,32 +1,41 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { UseFetch } from "./UseFetch";
 import { Follower } from "./components/Follower";
 
 function App() {
-  const {loading, data} = UseFetch()
-const [page, setPage] = useState(2);
-const [followers, setFollowers] = useState([]);
+  const { loading, data } = UseFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
 
-useEffect(()=>{
-  if(loading){
-    return
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    setFollowers(data[page]);
+  }, [loading, page]);
+  
+  const handlePage =(index) =>{
+   setPage(index)
+
   }
-setFollowers(data[page])
-}, [loading])
+
   return (
     <main>
-    <div className="app">
-      <h2>{loading ?"Loading..." :  "Pagination"} </h2>
-      <div className="underline"></div>
-    </div>
-    <section className="followers">
-      <div className="container">
-       {followers.map((x)=> {return<Follower key={x.id} {...x}/>})}
+      <div className="app">
+        <h2>{loading ? "Loading..." : "Pagination"} </h2>
+        <div className="underline"></div>
       </div>
-       <div className="btn-container">
-        {/* <button className="pgn">1</button> */}
-       </div>
-    </section>
+      <section className="followers">
+        <div className="container">
+          {followers.map((x) => {
+            return <Follower key={x.id} {...x} />;
+          })}
+        </div>
+        {!loading &&  <div className="btn-container">
+        {data.map((item, index)=> {return<button className={`pgn ${index===page ? "active": ""}`} key={index} onClick={()=>handlePage(index)}>{index +1}</button> })} 
+        </div>}
+       
+      </section>
     </main>
   );
 }
