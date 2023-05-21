a4b7014ca1a8ad6ab88a90fc57d783818584ecbb
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-21 15:50:00 +0200

    Update css and add useEffect and toggle theme

--- dark-mode/src/App.js
+++ dark-mode/src/App.js
@@ -1,19 +1,38 @@
+import { useEffect, useState } from "react";
 import { Article } from "./Article";
-import articles from "./data"; 
+import articles from "./data";
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+
   return (
- <main>
-<nav>
-  <div className="container">
-    <h1>Overreacted</h1>
-    <button className="btn">Toggle</button>
-  </div>
-</nav>
-{articles.map(x=> {
-  return <Article key={x.id} {...x}></Article>
-})}
- </main>
+    <main>
+      <nav>
+        <div className="container">
+          <h1>Overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            Toggle
+          </button>
+        </div>
+      </nav>
+      {articles.map((x) => {
+        return <Article key={x.id} {...x}></Article>;
+      })}
+    </main>
   );
 }
 
