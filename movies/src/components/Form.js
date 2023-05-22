99f0df5f648cdff759fa4b197263ff57f9606dfc
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-22 16:36:49 +0200

    Update search form-controlled  and using App context

--- movies/src/components/Form.js
+++ movies/src/components/Form.js
@@ -1,8 +1,18 @@
+import { useContext } from "react";
+import { AppContext } from "../context";
+
 export const Form = () => {
+  const { query, setQuery,error } = useContext(AppContext);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+  };
+
   return (
-  <form className="form-container">
-    <h2>Search Movies</h2>
-    <input type="text" />
-    <p>error</p>
-  </form>)
+    <form className="form-container" onSubmit={submitForm}>
+      <h2>Search Movies</h2>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} value={query} />
+      <p className="error">{error.show && error.msg }</p>
+    </form>
+  );
 };
