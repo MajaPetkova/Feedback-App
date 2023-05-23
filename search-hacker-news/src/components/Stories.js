40c0a0a607bb68b5996f83b7f59f89e04bc9623f
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-23 13:35:05 +0200

    Add reducer function, functionality for set stories and set loading, add spinner

--- search-hacker-news/src/components/Stories.js
+++ search-hacker-news/src/components/Stories.js
@@ -1,5 +1,19 @@
- export const Stories =()=>{
+import { useContext } from "react";
+import { AppContext } from "../context";
+import Spinner from "../assets/loading.png";
+
+export const Stories = () => {
+  const { isLoading, hits } = useContext(AppContext);
+
+  if (isLoading) {
     return (
-        <div>Stories</div>
-    )
-}
+      <div className="loading">
+        <img src={Spinner} alt="Loading..." />
+      </div>
+    );
+  }
+
+  return <div>
+    
+  </div>;
+};
--- search-hacker-news/src/context.js
+++ search-hacker-news/src/context.js
@@ -1,8 +1,15 @@
 import { createContext, useEffect, useReducer } from "react";
 import { reducer } from "./reducer";
+import { SET_LOADING, SET_STORIES } from "./actions";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
 
 const initialState = {
   isLoading: true,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
 };
 
 export const AppContext = createContext();
@@ -10,12 +17,26 @@ export const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const fetchStories = async () => {
-    dispatch({ type: "SET_LOADING" });
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING });
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+      //   console.log(data.hits);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   useEffect(() => {
-    fetchStories();
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, []);
 
-  return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
