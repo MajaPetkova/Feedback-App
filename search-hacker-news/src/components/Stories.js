a9cbdad01a3034eef3d1f2a58f2e6a45632ae7ee
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-23 14:09:57 +0200

    Display stories and add css

--- search-hacker-news/src/components/Stories.js
+++ search-hacker-news/src/components/Stories.js
@@ -13,7 +13,15 @@ export const Stories = () => {
     );
   }
 
-  return <div>
-    
-  </div>;
+  return <section className="stories-container">
+        {hits.map((x)=>{
+        const{title,objectID, url, num_comments, points, author } = x;
+    return <article className="story" key={objectID} >
+      <h4>{title}</h4>
+      <p className="info">{points} points by <span>{author} | </span>{num_comments} comments</p>
+      <a href={url} className="read_link" target="_blank" rel="noreferrer">read more</a>
+      <button className="remove-btn">remove</button>
+    </article>
+})}
+  </section>
 };
