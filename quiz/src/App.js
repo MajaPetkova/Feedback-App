afc3970da3eae77d5bffb0ec1a951ba260976e2f
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-24 19:19:45 +0200

    Add states in context and add components conditionally

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,8 +1,23 @@
-function App() {
-  return (
-    <div className="App">
+import { Loading } from "./components/Loading";
+import { SetupForm } from "./components/SetupForm";
+import { AppContext } from "./context";
+import { useContext } from "react";
 
-    </div>
+function App() {
+  const { isLoading, waiting, question, index, correct } = useContext(AppContext);
+
+  if(waiting) {
+    return <SetupForm/>
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <main>
+      <h2>Quiz starter</h2>
+    </main>
   );
 }
 
--- quiz/src/context.js
+++ quiz/src/context.js
@@ -1,7 +1,32 @@
-import {createContext} from "react";
+import { createContext, useState } from "react";
 
-export const AppContext= createContext();
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
-export const AppProvider =({children})=>{
-return <AppContext.Provider value="hello">{children}</AppContext.Provider>
-}
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [correct, setCorrect] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        isLoading,
+        questions,
+        correct,
+        index,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
