b2ce3fefc8ee277fbc4d1a1f22a73d314e321293
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-25 18:39:22 +0200

    Add error and about page and css for both

--- coctails-bar/src/pages/About.js
+++ coctails-bar/src/pages/About.js
@@ -1,5 +1,13 @@
-export const About =() =>{
-    return(
-        <div>About page</div>
-    )
-}
+export const About = () => {
+  return (
+    <section className="about-section">
+      <h1 className="title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        commodi dolor facilis, suscipit quisquam necessitatibus sapiente
+        mollitia dignissimos veritatis! Tempore laborum exercitationem mollitia
+        at quibusdam dolorum eaque tenetur obcaecati quo.
+      </p>
+    </section>
+  );
+};
--- coctails-bar/src/pages/Error.js
+++ coctails-bar/src/pages/Error.js
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom"
+
 export const Error =() =>{
     return(
-        <div>Error page</div>
+       <section className="error-section">
+        <h1 className="title">Oops it's a dead end!</h1>
+        <Link to="/" className="btn">Back home</Link>
+       </section>
     )
 }
