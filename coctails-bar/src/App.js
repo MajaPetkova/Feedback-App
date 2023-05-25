656e94502c89e08480d0adf66c977c45b7a96a1a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-25 17:26:05 +0200

    Add pages and components and routing
    add react-router-dom library

--- coctails-bar/src/App.js
+++ coctails-bar/src/App.js
@@ -1,8 +1,18 @@
+import {Route, Routes} from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Error } from "./pages/Error";
+import { SingleCocktail } from "./pages/SingleCocktail";
+
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+  <Routes>
+    <Route exact path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/cocktail/:id" element={<SingleCocktail/>}/>
+    <Route path="/*" element={<Error/>}/>
+  </Routes>
   );
 }
 
