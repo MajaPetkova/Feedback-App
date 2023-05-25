78afc8274c104d2e469f078bddc91ea564ee9d1b
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-25 18:12:38 +0200

    Add navbar, logo and css for navbar

--- coctails-bar/src/App.js
+++ coctails-bar/src/App.js
@@ -1,18 +1,21 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Error } from "./pages/Error";
 import { SingleCocktail } from "./pages/SingleCocktail";
-
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-  <Routes>
-    <Route exact path="/" element={<Home/>}/>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/cocktail/:id" element={<SingleCocktail/>}/>
-    <Route path="/*" element={<Error/>}/>
-  </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ coctails-bar/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/cocktails.jpg";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Cocktails Bar" />
+        </Link>
+        <h3>Cocktails Bar</h3>
+        </div>
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
