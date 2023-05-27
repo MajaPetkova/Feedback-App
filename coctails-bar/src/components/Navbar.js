f05fd2ba0332c35cadbc318e5c1dd3b7f6615377
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-27 08:29:51 +0200

    Add context and css for the components

--- coctails-bar/src/components/Navbar.js
+++ coctails-bar/src/components/Navbar.js
@@ -6,14 +6,18 @@ export const Navbar = () => {
     <nav className="navbar">
       <div className="nav-container">
         <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Cocktails Bar" />
-        </Link>
-        <h3>Cocktails Bar</h3>
+          <Link to="/">
+            <img src={logo} alt="Cocktails Bar" />
+          </Link>
+          <h3 className="logo-title">Cocktails Bar</h3>
         </div>
         <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </div>
     </nav>
--- quiz/README.md
+++ quiz/README.md
@@ -1,2 +1,2 @@
 <h1>Live Demo - Quiz</h1>
-<h2>quiz-be6f5f.netlify.app</h2>
+<h2>https://quiz-82b2e3.netlify.app</h2>
