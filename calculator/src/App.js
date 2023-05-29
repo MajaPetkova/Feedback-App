e4288b32288c5416e428881a6fc5e397a6977b29
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-29 12:23:54 +0200

    Display buttons for calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,8 +1,33 @@
-
 function App() {
+  const createDigits = () =>{
+    const arr=[];
+    for(let i=1; i<10; i++){
+      arr.push(<button key={i}>{i}</button>)
+    }
+    return arr;
+  }
+
+
   return (
     <div className="App">
-gg
+      <div className="calculator">
+        <div className="display">
+
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>-</button>
+          <button>+</button>
+          <button>DEL</button>
+        </div>
+        <div className="digits">
+         {createDigits()}
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
