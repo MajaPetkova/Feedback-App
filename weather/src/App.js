e745af2bcaea132738fc2ab59f1c0d15edc33ebf
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-31 11:59:02 +0200

    Fetching and display data

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,30 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [location, setLocation] = useState("London");
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+  const [data, setData] = useState({})
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+
+  const searchLocation = (e) =>{
+    if(e.key === "Enter"){
+      axios.get(url).then((response)=>{
+        setData(response.data)
+        console.log(response.data.name)
+      })
+      setLocation("")
+    }
+  }
+
   return (
     <div className="app">
       <div className="search">
-      <input type="text" name="" id="search"  placeholder="Enter location"  className="search"/>
+      <input type="text" placeholder="Enter location"  className="search" value={location} onChange={(e)=>setLocation( e.target.value)} onKeyPress={searchLocation}/>
       </div>
       <div className="container">
         <div className="top">
-        <div className="location"><h3>Berlin</h3></div>
-        <div className="temp"><h1>20 &#8451;</h1></div>
-        <div className="description"><h3>Clouds</h3></div>
+        <div className="location"><h3>{data.name}</h3></div>
+        <div className="temp">{data.main ? <h1>{((data.main.temp-32)*5/9).toFixed(1)} &#8451;</h1> : ""}</div>
+        <div className="description"><h3>{data.weather? data.weather[0].description :null}</h3></div>
         </div>
-
-        <div className="bottom">
-          <div className="feels"><p className="bold"> 22 &#8451; </p>
+       {data.name !== undefined && <div className="bottom">
+          <div className="feels"><p className="bold"> {data.main ?((data.main.feels_like- 32)*5/9).toFixed(1) : null} &#8451; </p>
           <p>Feels like</p></div>
-          <div className="humidity"><p className="bold">20 &#8451; </p>
+          <div className="humidity"><p className="bold">{data.main? data.main.humidity : null} % </p>
           <p>humidity</p></div>
-          <div className="wind"><p className="bold">12 MPh</p>
+          <div className="wind"><p className="bold">{data.wind ? data.wind.speed : null} MPH</p>
           <p>wind</p>
           </div>
-        </div>
+        </div>}
+      
       </div>
     </div>
   );
