839f3ad6f492ade3e29367fec4f307720aff0ac0
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-31 16:10:34 +0200

    Add state and jsx

--- bmi-app/src/App.js
+++ bmi-app/src/App.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="container">
-    
-  </div>;
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+  let imgSrc = "";
+
+  return (
+    <div className="container">
+      <h2>BMI Calculator</h2>
+      <form>
+        <div className="weight">
+          <label htmlFor="weight">Weight (kg) </label>
+          <input type="" name="weight" id="weight" value={weight} />
+        </div>
+        <div className="height">
+          <label htmlFor="height">Height (cm) </label>
+          <input type="" name="height" id="height" value={height} />
+        </div>
+        <div className="buttons">
+          <button className="btn">Submit</button>
+          <button className="btn">Clear</button>
+        </div>
+      </form>
+      <div className="center">
+        <h3>Your BMI is: {bmi}</h3>
+        <p>{message}</p>
+      </div>
+      <div className="img-container">
+        <img src={imgSrc} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
