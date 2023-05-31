901c711e92bf24348b720b68064261c5ed46f719
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-31 10:25:57 +0200

    Add css and update App

--- weather/src/App.js
+++ weather/src/App.js
@@ -5,15 +5,25 @@ function App() {
   const [location, setLocation] = useState("London");
   const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
   return (
-    <div className="App">
+    <div className="app">
+      <div className="search">
+      <input type="text" name="" id="search"  placeholder="Enter location"  className="search"/>
+      </div>
       <div className="container">
-        <div className="location"><p>Berlin</p></div>
-        <div className="temp"><h1>20 grad</h1></div>
-        <div className="description"><p>Clouds</p></div>
+        <div className="top">
+        <div className="location"><h3>Berlin</h3></div>
+        <div className="temp"><h1>20 &#8451;</h1></div>
+        <div className="description"><h3>Clouds</h3></div>
+        </div>
+
         <div className="bottom">
-          <div><p>Feels like : 24Grad</p></div>
-          <div className="humidity">20%</div>
-          <div className="wind">12 MPh</div>
+          <div className="feels"><p className="bold"> 22 &#8451; </p>
+          <p>Feels like</p></div>
+          <div className="humidity"><p className="bold">20 &#8451; </p>
+          <p>humidity</p></div>
+          <div className="wind"><p className="bold">12 MPh</p>
+          <p>wind</p>
+          </div>
         </div>
       </div>
     </div>
