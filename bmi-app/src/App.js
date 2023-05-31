e74786cdc637e0132c8102595f4336004bb48aaf
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-05-31 17:16:38 +0200

    Add assets and add logic for the calculator
    update css

--- bmi-app/src/App.js
+++ bmi-app/src/App.js
@@ -7,21 +7,66 @@ function App() {
   const [message, setMessage] = useState("");
   let imgSrc = "";
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (height == 0 || weight == 0) {
+      alert("Please enter a valid height and weight");
+    } else {
+      let bmi = weight / Math.pow(height / 100, 2);
+      setBmi(bmi.toFixed(1));
+      if (bmi < 18.5) {
+        setMessage("You are underweight");
+      } else if (bmi > 18.5 && bmi <= 24.9) {
+        setMessage("You are in the healthy range");
+      } else  {
+        setMessage("You are overweight");
+      }
+    }
+  };
+  if (bmi < 1) {
+    imgSrc = null;
+  } else if (bmi < 25) {
+    imgSrc = require("./assets/underweight.png");
+  } else if (bmi >= 25 && bmi < 30) {
+    imgSrc = require("./assets/healthy.png");
+  } else if (bmi > 30) {
+    imgSrc = require("./assets/overweight.png");
+  }
+
+  const reload = () => {
+    window.location.reload();
+  };
   return (
     <div className="container">
       <h2>BMI Calculator</h2>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="weight">
           <label htmlFor="weight">Weight (kg) </label>
-          <input type="" name="weight" id="weight" value={weight} />
+          <input
+            type=""
+            name="weight"
+            id="weight"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
         </div>
         <div className="height">
           <label htmlFor="height">Height (cm) </label>
-          <input type="" name="height" id="height" value={height} />
+          <input
+            type=""
+            name="height"
+            id="height"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
         </div>
         <div className="buttons">
-          <button className="btn">Submit</button>
-          <button className="btn">Clear</button>
+          <button className="btn" type="submit">
+            Submit
+          </button>
+          <button className="btn" type="submit" onClick={reload}>
+            Clear
+          </button>
         </div>
       </form>
       <div className="center">
