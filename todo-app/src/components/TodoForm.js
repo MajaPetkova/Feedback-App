3aa0534bef34c5d00b94b8878105828bec594711
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-03 18:36:25 +0200

    Update Form, added handleChange and handleSubmit

--- todo-app/src/components/TodoForm.js
+++ todo-app/src/components/TodoForm.js
@@ -1,10 +1,21 @@
 import {useState} from "react";
 
 export const TodoForm= ()=>{
-    const[input, setInput] = useState("")
+    const[input, setInput] = useState("");
+
+    const handleChange=(e)=>{
+        setInput(e.target.value)
+    }
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        setInput("")
+    }
     return(
         <div>
-            <input type="text" />
+            <form className="form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Add a todo" name= "text" className="form-input" value={input} onChange={handleChange}/>
+            <button className="btn">Add Todo</button>
+            </form>
         </div>
     )
 }
