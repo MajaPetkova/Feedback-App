14ec1ed8bcf29f7e08fdd92da0b75ca488ac09a2
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-06 22:53:34 +0200

    Add css and sign in form, state for form data and show password

--- market-place/src/pages/SignIn.jsx
+++ market-place/src/pages/SignIn.jsx
@@ -1,7 +1,68 @@
-export const SignIn =() =>{
-    return (
-        <div>
-            <h1>Sign In</h1>
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
+export const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const navigate = useNavigate();
+
+  const onChange = () => {};
+
+  return (
+    <>
+      <div className="page-container">
+        <header>
+          <p className="header">Welcome back!</p>
+        </header>
+        <main>
+          <form>
+            <div className="email-container">
+              <input
+                type="email"
+                className="emailInput"
+                placeholder="Email"
+                id="email"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
+            <div className="password-container">
+              <input
+                type={showPassword ? "text" : "passwords"}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={onChange}
+              />
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                onClick={() => setShowPassword(!showPassword)}
+                className="showPassword"
+              />
+            </div>
+            <div className="forgot-link">
+              <Link to="/forgot-password">Forgot Password</Link>
+            </div>
+            <div className="signInBar">
+              <p className="sighInText">Sign In</p>
+              <button className="btn signIn">
+                <ArrowRightIcon width="34px" fill="#ffffff" height="34px" />
+              </button>
+            </div>
+          </form>
+        </main>
+        <div className="register-link">
+          <Link to="/sign-up">Sign Up Instead</Link>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
