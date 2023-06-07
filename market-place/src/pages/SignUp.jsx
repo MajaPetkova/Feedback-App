73341b03bbf812ee3e9603d5108371a7bd04847a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-07 18:45:59 +0200

    Save user to firestore

--- market-place/src/pages/SignUp.jsx
+++ market-place/src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {db} from "../firebase.config";
+import {setDoc, doc, serverTimestamp} from "firebase/firestore"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -19,6 +22,28 @@ export const SignUp = () => {
       [e.target.id]: e.target.value,
     }));
   };
+  const submitHandler=async(e)=>{
+   e.preventDefault();
+    try{
+    const auth= getAuth();
+    const userCredential=  await createUserWithEmailAndPassword(auth, email, password);
+    const user= userCredential.user;
+
+    updateProfile(auth.currentUser, {
+        displayName:name
+    })
+    const formDataCopy = {...formData};
+    delete formDataCopy.password;
+    formDataCopy.timeStamp= serverTimestamp();
+
+    await setDoc(doc(db, "users", user.uid), formDataCopy)
+
+    navigate("/")
+    }catch(err){
+        console.log(err)
+    }
+
+  }
 
   return (
     <>
@@ -27,7 +52,7 @@ export const SignUp = () => {
           <p className="header">Welcome back!</p>
         </header>
         <main>
-          <form>
+          <form onSubmit={submitHandler}>
           <div className="name-container">
               <input
                 type="name"
