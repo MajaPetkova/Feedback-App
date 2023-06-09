deedb3625e0c170c66fb161a21a39a8029ab500b
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-09 09:14:14 +0200

    Add Spinner and css for the Spinner

--- market-place/src/components/PrivateRoute.jsx
+++ market-place/src/components/PrivateRoute.jsx
@@ -1,10 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStatus } from "../hooks/useAuthStatus";
+import { Spinner } from "./Spinner";
 
 export const PrivateRoute = () => {
-const {loggedIn, checkInStatus} = useAuthStatus()
-if(checkInStatus){
-    return <h3>Loading...</h3>
-}
+  const { loggedIn, checkInStatus } = useAuthStatus();
+
+
+  if (checkInStatus) {
+    return <Spinner/>;
+  }
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
