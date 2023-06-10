71f6431fdd10168d730d116e3069675e1d1da064
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-10 13:39:33 +0200

    Add explore page and css for explore page

--- market-place/src/pages/Explore.jsx
+++ market-place/src/pages/Explore.jsx
@@ -1,7 +1,26 @@
-export const Explore =() =>{
-    return (
-        <div>
-            <h1>Explore</h1>
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+
+export const Explore = () => {
+  return (
+    <div className="explore-container">
+      <header>
+        <p className="header">Explore</p>
+      </header>
+      <main>
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+            <Link to="/category/rent">
+                <img src={rentCategoryImage} alt="rent-img"  className="explore-img"/>
+                <p className="explore-name">Places for rent</p>
+            </Link>
+            <Link to="/category/sale">
+                <img src={sellCategoryImage} alt="sale-img" className="explore-img" />
+                <p className="explore-name">Places for sale</p>
+            </Link>
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
