e0abd448da29ed0ed537042155007a7c03a97267
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-10 12:54:30 +0200

    Add google Authentication

--- market-place/src/components/OAuth.jsx
+++ market-place/src/components/OAuth.jsx
@@ -1,6 +1,6 @@
 import { useLocation, navigate, useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import googleIcon from "../assets/svg/googleIcon.svg";
@@ -11,8 +11,26 @@ export const OAuth = () => {
 
   const onGoogleClick = async () => {
     try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check for user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      // if user doesn't exist
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, "users", user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
     } catch (err) {
-        
+      toast.error("Could not authorize with google");
     }
   };
   return (
