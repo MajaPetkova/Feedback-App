3e56686fd0a78e2eddc7161fa9095143c80491fc
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-11 10:17:16 +0200

    Update spinner with css

--- market-place/src/components/Spinner.jsx
+++ market-place/src/components/Spinner.jsx
@@ -1,9 +1,7 @@
-import spinner from "../assets/jpg/loading.gif"
-
-export const Spinner =()=>{
-    return (
-        <div className="spinner-container">
-           <img src={spinner} alt="loading..." />
-        </div>
-    )
-}
+export const Spinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner-ring"></div>
+    </div>
+  );
+};
