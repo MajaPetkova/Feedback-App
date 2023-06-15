fd817f7db553f8f3920a21f3f3a9de5247589156
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-15 16:02:10 +0200

    Add json server library, add state and jsx and fetch data

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [columns, setColumns] = useState([]);
+  const [records, setRecords] = useState([]);
+
+  useEffect(() =>{
+ fetch("http://localhost:3030/users")
+ .then(res=> res.json())
+ .then(data =>{setColumns(Object.keys(data[0]))
+                setRecords(data)
+  })
+  // console.log(columns)
+  // console.log(records)
+  }, []);
+
   return (
-    <div className="">
-      
+    <div className="container">
+      <table className="table">
+        <thead className="head">
+          <tr>
+          {columns.map((x, i)=> (
+            <th key={i}>{x}</th>
+            ))}
+            <th>action</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+            {records.map((x, i)=>(
+              <tr key={i}>
+              <td>{x.id}</td>
+              <td>{x.name}</td>
+              <td>{x.email}</td>
+              <td>Edit/Delete</td>
+              </tr>
+            ))}
+        
+        </tbody>
+      </table>
     </div>
   );
 }
