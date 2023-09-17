32abd660ae00ae696d8da92c7ad2a0668a66ce2a
Maja Petkova <majapetkova1@yahoo.com>
2023-09-17 14:10:52 +0200

    Update featured jsx and scss

--- admin-panel/src/components/featured/Featured.jsx
+++ admin-panel/src/components/featured/Featured.jsx
@@ -1,7 +1,9 @@
 import "./featured.scss";
+import "react-circular-progressbar/dist/styles.css"
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import {CircularProgressbar} from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css"
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 
 function Featured() {
   return (
@@ -12,7 +14,33 @@ function Featured() {
         </div>
         <div className="bottom">
             <div className="featuredChart">
-                <CircularProgressbar value={70} text={"70%"} strokeWidth={4} />
+                <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+            </div>
+            <p className="title">Total sales made today</p>
+            <p className="amount">$4200</p>
+            <p className="desc">Previous transactions processing. Last payments may not be included.</p>
+            <div className="summary">
+                <div className="item">
+                    <div className="title">Target</div>
+                    <div className="itemResult negative">
+                        <KeyboardArrowDownOutlinedIcon fontSize="small"/>
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="title">Last Week</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowDownOutlinedIcon fontSize="small"/>
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="title">Last Month</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
