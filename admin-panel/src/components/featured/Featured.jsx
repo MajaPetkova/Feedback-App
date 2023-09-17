35d66ad0f59607842d4a24cf0234410cd5f9f9bc
Maja Petkova <majapetkova1@yahoo.com>
2023-09-17 13:33:57 +0200

    Add react-circular-progressbar library for circular progress and add to featured comp.

--- admin-panel/src/components/featured/Featured.jsx
+++ admin-panel/src/components/featured/Featured.jsx
@@ -1,5 +1,7 @@
 import "./featured.scss";
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import {CircularProgressbar} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css"
 
 function Featured() {
   return (
@@ -8,7 +10,11 @@ function Featured() {
             <h1 className="title">Total Revenue</h1>
             <MoreVertOutlinedIcon fontSize="small"/>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+            <div className="featuredChart">
+                <CircularProgressbar value={70} text={"70%"} strokeWidth={4} />
+            </div>
+        </div>
     </div>
   )
 }
