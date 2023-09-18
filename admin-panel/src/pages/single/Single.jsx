7af65005772b25cafb1f624fd04c5e18cc12c4ec
Maja Petkova <majapetkova1@yahoo.com>
2023-09-18 19:40:35 +0200

    Add Single jsx and scss

--- admin-panel/src/pages/single/Single.jsx
+++ admin-panel/src/pages/single/Single.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./single.scss";
 
 function Single() {
   return (
-    <div>Single</div>
-  )
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editBtn">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" className="itemImg" />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@yahoo.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+1 7854 44 777</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Main str. 123 New York</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div> 
+            
+            </div>
+          </div>
+          <div className="right"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Single
+export default Single;
