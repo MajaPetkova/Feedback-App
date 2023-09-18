e89feaf80c6347e8bf122cbdfaf32afeb6718d08
Maja Petkova <majapetkova1@yahoo.com>
2023-09-18 18:04:52 +0200

    Update dataTable.scss and add action column to the table in data table

--- admin-panel/src/components/dataTable/DataTable.jsx
+++ admin-panel/src/components/dataTable/DataTable.jsx
@@ -1,21 +1,39 @@
 import "./dataTable.scss";
-import { DataGrid } from '@mui/x-data-grid';  
+import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../dataTableSource";
 
-const DataTable =()=> {
-  return (
-    <div className="dataTable">  <DataGrid
-    rows={userRows}
-    columns={userColumns}
-    initialState={{
-      pagination: {
-        paginationModel: { page: 0, pageSize: 5 },
+const DataTable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewBtn"> View</div>
+            <div className="deleteBtn"> Delete</div>
+          </div>
+        );
       },
-    }}
-    pageSizeOptions={[5, 10]}
-    checkboxSelection
-  /></div>
-  )
-}
+    },
+  ];
 
-export default DataTable
+  return (
+    <div className="dataTable">
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default DataTable;
