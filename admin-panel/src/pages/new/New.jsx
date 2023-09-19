bb51eea0aaa26a8dd3b01b6ab2d177b23f361b56
Maja Petkova <majapetkova1@yahoo.com>
2023-09-19 19:13:45 +0200

    Update image and add handler on file input

--- admin-panel/src/pages/new/New.jsx
+++ admin-panel/src/pages/new/New.jsx
@@ -4,8 +4,9 @@ import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUpload
 import "./new.scss";
 import { useState } from "react";
 
-function New({title, inputs}) {
+function New({ title, inputs }) {
   const [file, setFile] = useState("");
+
   return (
     <div className="new">
       <Sidebar />
@@ -17,25 +18,32 @@ function New({title, inputs}) {
         <div className="bottom">
           <div className="left">
             <img
-              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              src={file ? URL.createObjectURL(file) :"https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
               alt="img"
             />
           </div>
           <div className="right">
             <form>
-              <div className="formInput" >
-                <label htmlFor="file">Image <DriveFolderUploadOutlinedIcon className="icon"/></label>
-                <input type="file" id="file" style={{display:"none"}}/>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                  onChange={(e) =>setFile( e.target.files[0])}
+                />
               </div>
-              {inputs.map((x) =>
-              <div className="formInput" key={inputs.id}>
-                <label>{x.label}</label>
-                <input type={x.type} placeholder={x.placeholder} />
+              {inputs.map((x) => (
+                <div className="formInput" key={x.id}>
+                  <label>{x.label}</label>
+                  <input type={x.type} placeholder={x.placeholder} />
+                </div>
+              ))}
+              <div className="formInput">
+                <button>Send</button>
               </div>
-              )}
-               <div className="formInput">
-              <button>Send</button>
-               </div>
             </form>
           </div>
         </div>
