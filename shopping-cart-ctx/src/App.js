573ab644ef53df6ef818efca62b4a3da8bf954d9
Maja Petkova <majapetkova1@yahoo.com>
2023-09-22 13:02:07 +0200

    Add react-router-dom lib, routes and links

--- shopping-cart-ctx/src/App.js
+++ shopping-cart-ctx/src/App.js
@@ -1,10 +1,20 @@
-
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Cart from "./components/cart/Cart";
+import Products from "./components/products/Products";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+   <div>useReducer Hook and useContext Project</div>
+   <>
+   <Link to="/">Home</Link>
+   <Link to="/cart">Cart</Link>
+   </>
+   <Routes>
+   <Route path="/" element={<Products/>}></Route>
+   <Route path="/cart" element={<Cart/>}></Route>
+   </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ shopping-cart-ctx/src/components/cart/Cart.jsx
@@ -0,0 +1,10 @@
+import "./cart.css";
+
+
+function Cart() {
+  return (
+   <h1>Your cart total is 300.00$</h1>
+  )
+}
+
+export default Cart
