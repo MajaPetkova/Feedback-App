7889a8c27668b428e69a7826ca207787ea148662
Maja Petkova <majapetkova1@yahoo.com>
2023-09-26 16:54:06 +0200

    Update UI

--- budget-planner/src/components/ExpenseItem.js
+++ budget-planner/src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
+import {TiDelete} from "react-icons/ti";
+import React from "react"
 
-
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div>ExpenseItem</div>
-  )
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      {props.name}
+      <div>
+        <span className="mr-3">
+        {props.cost}$
+        </span>
+        <TiDelete size="1.5em"></TiDelete>
+      </div>
+    </li>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
--- budget-planner/src/components/ExpenseList.js
+++ budget-planner/src/components/ExpenseList.js
@@ -3,16 +3,16 @@ import ExpenseItem from "./ExpenseItem";
 function ExpenseList() {
   const expenses = [
     { id: 123456789, name: "Shopping", cost: 50 },
-    { id: 123456789, name: "Holiday", cost: 350 },
-    { id: 123456789, name: "Transportation", cost: 78 },
-    { id: 123456789, name: "Fuel", cost: 60 },
-    { id: 123456789, name: "Child Care", cost: 500 },
+    { id: 123446789, name: "Holiday", cost: 350 },
+    { id: 13456789, name: "Transportation", cost: 78 },
+    { id: 123456589, name: "Fuel", cost: 60 },
+    { id: 123456779, name: "Child Care", cost: 500 },
   ];
   return (
-    <ul>
-      {expenses.map((x) => (
+    <ul className="list-group">
+      {expenses.map((x) => 
         <ExpenseItem key={x.id} name={x.name} cost={x.cost}/>
-      ))}
+      )}
     </ul>
   );
 }
