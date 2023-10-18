1548814c6455cbf78e5a45df05e6c179ee089f24
Maja Petkova <majapetkova1@yahoo.com>
2023-10-18 14:13:56 +0200

    Make the form controlled with state and onChange

--- budget-planner/src/components/AddExpenseForm.js
+++ budget-planner/src/components/AddExpenseForm.js
@@ -1,14 +1,19 @@
 import React from "react";
+import {useState} from "react";
+
 
 function AddExpenseForm() {
+    const [name, setName] = useState("");
+    const[ cost, setCost] = useState("");
+
   return <form className="row">
     <div className="col-sm">
         <label htmlFor="name">Name</label>
-        <input type="text" required="required" className="form-control" id="name" />
+        <input type="text" required="required" className="form-control" id="name" value={name} onChange={(e)=>setName(e.target.value)}/>
     </div>
     <div className="col-sm">
     <label htmlFor="cost">Cost</label> 
-    <input type="text"required="required" className="form-control" id="cost"  />
+    <input type="text"required="required" className="form-control" id="cost" value={cost} onChange={(e)=>setCost(e.target.value)} />
     </div>
     <div className="row mt-3">
     <div className="col-sm">
