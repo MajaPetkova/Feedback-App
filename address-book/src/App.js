2679ef99959c55aad97825fafdb592bc25e63af4
Maja Petkova <majapetkova1@yahoo.com>
2023-11-17 21:13:11 +0100

    Initialize new react project - address book

---
+++ address-book/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+}
+
+export default App;
