fc3e17b883f42e88542de9a708044a19a68d584b
Maja Petkova <majapetkova1@yahoo.com>
2023-11-18 23:14:48 +0100

    Add handleAddFormChange function  and state

--- address-book/src/App.js
+++ address-book/src/App.js
@@ -4,7 +4,22 @@ import { useState } from "react";
 
 function App() {
   const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    fullName: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+  });
 
+  const handleAddFormChange = (e) => {
+    e.preventDefault();
+    const fieldName = e.target.getAttribute("name");
+    const fieldValue = e.target.value;
+
+    const newFormData = {...addFormData}
+    newFormData[fieldName] = fieldValue;
+    setAddFormData(newFormData)
+  };
 
   return (
     <div className="app-container">
@@ -18,21 +33,49 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {contacts.map(x => (<tr>
-            <th>{x.fullName}</th>
-            <th>{x.address}</th>
-            <th>{x.phoneNumber}</th>
-            <th>{x.email}</th>
-          </tr>))}
+          {contacts.map((x) => (
+            <tr>
+              <th>{x.fullName}</th>
+              <th>{x.address}</th>
+              <th>{x.phoneNumber}</th>
+              <th>{x.email}</th>
+            </tr>
+          ))}
         </tbody>
       </table>
       <h2>Add a Contact</h2>
-      <form>
-      <input type="text" name="fullName" required="required" placeholder="Enter a name..."/>
-      <input type="text" name="address" required="required" placeholder="Enter an address..."/>
-      <input type="text" name="phoneNumber" required="required" placeholder="Enter a phone number..."/>
-      <input type="email" name="email" required="required" placeholder="Enter an email...."/>
-      <button type="submit">Add</button>
+      <form >
+        <input
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder="Enter a name..."    
+        onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="address"
+          required="required"
+          placeholder="Enter an address..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="phoneNumber"
+          required="required"
+          placeholder="Enter a phone number..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="email"
+          name="email"
+          required="required"
+          placeholder="Enter an email...."
+          onChange={handleAddFormChange}
+        />
+        <button type="submit" >
+          Add
+        </button>
       </form>
     </div>
   );
