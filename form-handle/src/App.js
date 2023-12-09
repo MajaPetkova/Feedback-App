02001660082142e38ea11b17737416aacf23eb88
Maja Petkova <majapetkova1@yahoo.com>
2023-12-09 15:00:25 +0100

    Handling form with many inputs and add css for the app

--- form-handle/src/App.js
+++ form-handle/src/App.js
@@ -1,14 +1,66 @@
 import { FormInput } from "./components/FormInput";
-
+import { useState } from "react";
 
 function App() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: " Confirm password",
+      label: "Confirm password",
+    },
+  ];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) =>{
+    setValues({...values,  [e.target.name]: e.target.value})
+  }
+  console.log(values)
   return (
     <div className="app">
-      <form>
-        <FormInput placeholder= "Username"/>
-         <FormInput placeholder="email"/>
-         <FormInput placeholder = "full name"/>
-         <FormInput placeholder="something else"/>
+      <form onSubmit={handleSubmit}>
+        {inputs.map((x)=> (<FormInput key ={x.id} {...x} value={values[x.name]} onChange={onChange}/>))}
+       
+        <button>Submit</button>
       </form>
     </div>
   );
