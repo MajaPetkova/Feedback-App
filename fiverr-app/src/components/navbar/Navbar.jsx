503e85decf808cba6ad32de1744e75e57c518ff9
Maja Petkova <majapetkova1@yahoo.com>
2023-12-16 11:52:27 +0100

    Install react-router-dom and add menu to the navbar and css for the menu

--- fiverr-app/src/components/navbar/Navbar.jsx
+++ fiverr-app/src/components/navbar/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import "./navbar.scss"
+import React from "react";
+import "./navbar.scss";
+// import { Link } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className="container">
-            <div className="logo">
-                <span className='text'>Feverr</span>
-                <span className='dot'>.</span>
-            </div>
-            <div className="links">
-               <span>Fiverr Business</span>
-               <span>Explore</span>
-               <span>English</span>
-               <span>Sign In</span>
-               <span>Become a seller</span> 
-               <button>Join</button>
-            </div>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">Fiverr</span>
+          {/* </Link> */}
+
+          <span className="dot">.</span>
         </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign In</span>
+          <span>Become a seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      <hr />
+       <div className="menu">
+        <span>Test</span>
+        <span>Test2</span>
+       </div>
     </div>
-  )
-}
+  );
+};
