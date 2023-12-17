1f61030bd30a2ae00a641ebdae2c295f755d0907
Maja Petkova <majapetkova1@yahoo.com>
2023-12-17 18:25:32 +0100

    Add user and condition rendering if has user and add scss

--- fiverr-app/src/components/navbar/Navbar.jsx
+++ fiverr-app/src/components/navbar/Navbar.jsx
@@ -4,14 +4,24 @@ import "./navbar.scss";
 
 export const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
   };
   useEffect(() => {
     window.addEventListener("scroll", isActive);
 
-    return() => {window.removeEventListener("scroll", isActive)}
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
   }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
   return (
     <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
@@ -27,8 +37,25 @@ export const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign In</span>
-          <span>Become a seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <span>{currentUser?.username}</span>
+             {open && <div className="options">
+                {currentUser?.isSeller && (
+                  <>
+                    <span>Gigs</span>
+                    <span>Add New Gig</span>
+                  </>
+                )}
+                <span>Orders</span>
+                <span>Messages</span>
+                <span>Logout</span>
+              </div>}
+            </div>
+          )}
         </div>
       </div>
       {active && (
