dc98fef5837cd4accf333ea3390fd34e0959cbea
Maja Petkova <majapetkova1@yahoo.com>
2023-12-27 15:04:29 +0100

    Add data file, and new components and scss files, install carousel library

---
+++ fiverr-app/src/components/catCard/catCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./catCard.scss";
+import { Link } from "react-router-dom";
+
+export const catCard = ({ item }) => {
+  return (
+    <Link to="/gig/123">
+      <div className="catCard">
+      
+      </div>
+    </Link>
+  );
+};
---
+++ fiverr-app/src/components/slide/Slide.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./slide.scss";
+import Carousal from "@itseasy21/react-elastic-carousel";
+
+export const Slide = () => {
+  return (
+    <div className="slide">
+      <div className="container"></div>
+    </div>
+  );
+};
