d306f4d0f9551e2d60f7ea3b87fd14ec93d0ef2d
Maja Petkova <majapetkova1@yahoo.com>
2023-12-30 19:19:19 +0100

    Update gigs page and create gigCard component

--- fiverr-app/src/components/gigCard.jsx/GigCard.jsx
+++ fiverr-app/src/components/gigCard.jsx/GigCard.jsx
@@ -4,21 +4,32 @@ import { Link } from "react-router-dom";
 
 export const GigCard = ({ item }) => {
   return (
-    <Link to="/gig/123456">
+    <Link to="/gig/123">
       <div className="gigCard">
         <div className="container">
           <img src={item.img} alt="" />
           <div className="info">
             <div className="user">
-                <img src={item.pp} alt="" />
-                <span>{item.username}</span>
+              <img src={item.pp} alt="" />
+              <span>{item.username}</span>
             </div>
             <p>{item.desc}</p>
             <div className="star">
-                
+              <img src="./images/star.png" alt="" />
+              <span>{item.star}</span>
+            </div>
+            <p>{item.desc}</p>
+            <div className="star">
+              <img src="./images/star.png" alt="" />
+              <span>{item.star}</span>
             </div>
           </div>
-          <div className="details"></div>
+          <hr />
+          <div className="details">
+            <img src="./images/heart.png" alt="" />
+            <span>STARTING AT</span>
+            <h2>{item.price}</h2>
+          </div>
         </div>
       </div>
     </Link>
--- fiverr-app/src/pages/gigs/Gigs.jsx
+++ fiverr-app/src/pages/gigs/Gigs.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import "./gigs.scss"
+import React from "react";
+import "./gigs.scss";
 
 export const Gigs = () => {
   return (
-    <div>Gigs</div>
-  )
-}
-
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">FIVERR > GRAPHICS & DESIGN > </span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Fiverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
