f90ba27b4f9f4bbc2be506f73f7c13c07a5c02ff
Maja Petkova <majapetkova1@yahoo.com>
2023-12-31 14:05:18 +0100

    Update gigCard and add scss

--- fiverr-app/src/components/gigCard.jsx/GigCard.jsx
+++ fiverr-app/src/components/gigCard.jsx/GigCard.jsx
@@ -4,36 +4,32 @@ import { Link } from "react-router-dom";
 
 export const GigCard = ({ item }) => {
   return (
-    <Link to="/gig/123">
-      <div className="gigCard">
-        <div className="container">
-          <img src={item.img} alt="" />
-          <div className="info">
-            <div className="user">
-              <img src={item.pp} alt="" />
-              <span>{item.username}</span>
-            </div>
-            <p>{item.desc}</p>
-            <div className="star">
-              <img src="./images/star.png" alt="" />
-              <span>{item.star}</span>
-            </div>
-            <p>{item.desc}</p>
-            <div className="star">
-              <img src="./images/star.png" alt="" />
-              <span>{item.star}</span>
-            </div>
-          </div>
-          <hr />
-          <div className="details">
-            <img src="./images/heart.png" alt="" />
-            <div className="price">
-              <span>STARTING AT</span>
-              <h2>{item.price}</h2>
-            </div>
-          </div>
+    <Link to="/gig/123" className="link">
+    <div className="gigCard">
+      <img src={item.img} alt="" />
+      <div className="info">
+        <div className="user">
+          <img src={item.pp} alt="" />
+          <span>{item.username}</span>
+        </div>
+        <p>{item.desc}</p>
+        <div className="star">
+          <img src="./images/star.png" alt="" />
+          <span>{item.star}</span>
         </div>
       </div>
-    </Link>
+      <hr />
+      <div className="details">
+        <img src="./images/heart.png" alt="" />
+        <div className="price">
+          <span>STARTING AT</span>
+          <h2>
+            $ {item.price}
+            <sup>99</sup>
+          </h2>
+        </div>
+      </div>
+    </div>
+  </Link>
   );
 };
