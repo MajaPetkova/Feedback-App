63cdd867c99f703bcf8d1d04d334de45a863a9d5
Maja Petkova <majapetkova1@yahoo.com>
2024-01-03 12:09:30 +0100

    Add template and style to messages page

--- fiverr-app/src/pages/messages/Messages.jsx
+++ fiverr-app/src/pages/messages/Messages.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
-import "./messages.scss"
+import React from "react";
+import "./messages.scss";
 
 export const Messages = () => {
-  return (
-    <div>Messages</div>
-  )
-}
+  const message = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, provident fuga! Porro consequuntur nulla, alias error facere, maiores enim similique nihil facilis nam eligendi aliquam laborum, mollitia impedit commodi eius obcaecati aut distinctio exercitationem dolore nesciunt? Ipsum, hic?"
+      return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>{message.substring(0,100)}...</td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>{message.substring(0,100)}...</td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>{message.substring(0,100)}...</td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>{message.substring(0,100)}...</td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+           John Doe
+            </td>
+            <td>{message.substring(0,100)}...</td>
+            <td>1 day ago</td>
+            <td><button>Mark as Read</button></td>
+          
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
