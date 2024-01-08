be7cb139f69fb123b735c91f98ce1b14b76ad836
Maja Petkova <majapetkova1@yahoo.com>
2024-01-08 12:00:10 +0100

    create models for gig and user

---
+++ fiverr-app/api/models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const { Schema, model, Types:{ObjectId} } = mongoose;
+
+const UserSchema = new Schema({
+  username: {type:String, required:true,unique:true},
+  email: {type:String, required:true,unique:true},
+  password: {type:String, required:true},
+  img: {type:String, required:false}, 
+  country: {type:String, required:true},  
+  phone: {type:String, required:false},  
+  desc:{ type:String, required:false},
+  isSeller:{type:Boolean,default:false}
+});
+
+const User = model("User", UserSchema);
+module.exports= User;
