326506cc69a936a7338c4ff20d0ad8b303b7651a
Maja Petkova <majapetkova1@yahoo.com>
2024-01-13 14:58:09 +0100

    Add token and cookie when the user log in

--- fiverr-app/api/controllers/authController.js
+++ fiverr-app/api/controllers/authController.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+
+// const secretKey= "hgdghfdkjbdfhgfdjgbdfjgkdfnmdf"
 
 const register = async (req, res) => {
   try {
@@ -22,14 +25,22 @@ const login = async (req, res) => {
     if(!user){
        return res.status(404).send("User not found")
     }
-    const isCorrect = bcrypt.compare(req.body.password, user.password )
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password )
     if(!isCorrect){
       return res.status(400).send("Username or password are not correct")
     }
+    const token= jwt.sign({
+      id: user._id,
+      isSeller: user.isSeller
+    }, process.env.JWT_KEY)
+
     const {password, ...info} = user._doc;
-    res.status(200).send(info)
+    res.cookie("accessToken", token, {
+      httpOnly: true,
+    }).status(200).send(info)
+    // res.status(200).send(info)
   } catch (err) {
-    res.status(404).send("Something went wrong");
+     res.status(404).send("Something went wrong");
   }
 };
 const logout = (req, res) => {
