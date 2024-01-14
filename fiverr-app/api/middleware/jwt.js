2670ee23712ca9167c8bf98f5820b8d1a355a556
Maja Petkova <majapetkova1@yahoo.com>
2024-01-14 19:28:20 +0100

    Create createError function and update middleware jwt

--- fiverr-app/api/middleware/jwt.js
+++ fiverr-app/api/middleware/jwt.js
@@ -1,16 +1,20 @@
 const jwt = require("jsonwebtoken");
+const createError = require("../utils/createError");
 
-const verifyToken = (req, res) => {
+const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) {
-    res.status(401).send("You are not authenticated");
+    // res.status(401).send("You are not authenticated");
+   return next(createError(401, "You are not authenticated"))
   }
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) {
-      return res.status(403).send("Token is not valid!");
+    //   return res.status(403).send("Token is not valid!");
+     return next(createError(403, "Token is not valid!"))
     }
     req.userId = payload.id;
     req.isSeller = payload.isSeller;
+    next();
   });
 };
 module.exports = verifyToken;
