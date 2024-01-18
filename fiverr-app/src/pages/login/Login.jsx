4d47707a3128dff1127f8b8da700dd5fe6a17f59
Maja Petkova <majapetkova1@yahoo.com>
2024-01-18 22:32:22 +0100

    fething data using axios, error handling and setting user in localStorage

--- fiverr-app/src/pages/login/Login.jsx
+++ fiverr-app/src/pages/login/Login.jsx
@@ -1,31 +1,38 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./login.scss";
+import axios from "axios";
 
 export const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const userData = { username: username, password: password };
+    // const userData = { username:username, password: password };
     // console.log(userData)
     try {
-      fetch("http://localhost:8000/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData)
-      })
-        .then((res) => res.json())
-        .then((data) => console.log(data));
-
-      // const data = await res.json();
-      // console.log(res);
+      // const res = await Fetch("http://localhost:8000/api/auth/login", {
+      //   method: "POST",
+      //   mode: "cors",
+      //   credentials: "include",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({ username, password }),
+      // });
+      const res = await axios.post("http://localhost:8000/api/auth/login", {
+        username,
+        password,
+      });
+      console.log(res.data);
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/")
     } catch (err) {
-      setError(err);
-      // console.log(err);
+      setError(err.response.data)
+      // console.log(err.response.data);
     }
   };
 
@@ -48,6 +55,7 @@ export const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit">Login</button>
+        {/* {error && error} */}
       </form>
     </div>
   );
