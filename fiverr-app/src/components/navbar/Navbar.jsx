e07445418d142ba4fa2d92c1f44d59d3382cdd5e
Maja Petkova <majapetkova1@yahoo.com>
2024-01-22 12:24:53 +0100

    Add logout functionality and navigate to home page

--- fiverr-app/src/components/navbar/Navbar.jsx
+++ fiverr-app/src/components/navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./navbar.scss";
-import { Link, useLocation } from "react-router-dom";
-import { Login } from "../../pages/login/Login";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Navbar = () => {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
+  const navigate= useNavigate()
 
-  const {pathname} = useLocation()
+  const { pathname } = useLocation();
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
   };
@@ -19,7 +20,17 @@ export const Navbar = () => {
     };
   }, []);
 
-  const currentUser = JSON.parse(localStorage.getItem("currentUser"))
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const handleLogout = async () => {
+    try {
+      await axios.post("http://localhost:8000/api/auth/logout")
+      localStorage.setItem("currentUser", null);
+      navigate("/")
+    } catch (err) {
+      console.log(err)
+    }
+  };
 
   return (
     <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
@@ -35,15 +46,14 @@ export const Navbar = () => {
           <span>Fiverr Business</span>
           <span>Explore</span>
           <span>English</span>
-          <Link to="/login" className="link">Sign In</Link>
+          <Link to="/login" className="link">
+            Sign In
+          </Link>
           {!currentUser?.isSeller && <span>Become a seller</span>}
           {!currentUser && <button>Join</button>}
           {currentUser && (
             <div className="user" onClick={() => setOpen(!open)}>
-              <img
-                src={currentUser.img || "/images/noavatar.jpg" }
-                alt=""
-              />
+              <img src={currentUser.img || "/images/noavatar.jpg"} alt="" />
               <span>{currentUser?.username}</span>
               {open && (
                 <div className="options">
@@ -63,7 +73,7 @@ export const Navbar = () => {
                   <Link to="/messages" className="link">
                     Messages
                   </Link>
-                  <Link to="/logout" className="link">
+                  <Link to="/logout" className="link" onClick={handleLogout}>
                     Logout
                   </Link>
                 </div>
@@ -72,19 +82,39 @@ export const Navbar = () => {
           )}
         </div>
       </div>
-      {(active || pathname !== "/")  && (
+      {(active || pathname !== "/") && (
         <>
           <hr />
           <div className="menu">
-           <Link className="link menuLink" to="/">Graphics & Design</Link>
-           <Link className="link menuLink" to="/"> video & Animation</Link>
-           <Link className="link menuLink" to="/">Writing & Translation</Link>
-           <Link className="link menuLink" to="/"> AI Services</Link>
-           <Link className="link menuLink" to="/">Digital Marketing</Link>
-           <Link className="link menuLink" to="/">Music & Audio</Link>
-           <Link className="link menuLink" to="/">Programming & Tech</Link>
-           <Link className="link menuLink" to="/">Business</Link>
-           <Link className="link menuLink" to="/">Lifestyle</Link>
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              {" "}
+              video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              {" "}
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
           </div>
           <hr />
         </>
