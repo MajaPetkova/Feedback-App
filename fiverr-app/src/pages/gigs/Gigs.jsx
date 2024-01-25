b5bab13854413a69b1ed772468dd56de3b1c2425
Maja Petkova <majapetkova1@yahoo.com>
2024-01-25 13:34:47 +0100

    Add useRef, and update fetch-search

--- fiverr-app/src/pages/gigs/Gigs.jsx
+++ fiverr-app/src/pages/gigs/Gigs.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect} from "react";
 import "./gigs.scss";
 import { gigs } from "../../data";
 import { GigCard } from "../../components/gigCard.jsx/GigCard";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
+import { useLocation } from "react-router-dom";
 
 export const Gigs = () => {
   const [open, setOpen] = useState(false);
   const [sort, setSort] = useState("sales");
+  const minRef = useRef();
+  const maxRef = useRef();
+
+
+  const { search } = useLocation();
+
   const reSort = (type) => {
     setSort(type);
     setOpen(false);
   };
-  
-  const { isPending, error, data } = useQuery({
+
+  const { isLoading, error, data, refetch } = useQuery({
     queryKey: ["repoData"],
-    queryFn: () => newRequest("/gigs"),
+    queryFn: () =>
+      newRequest
+        .get(
+          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}`)
+        .then((res) => {
+          return res.data;
+        }),
   });
   console.log(data);
-  
 
+  const apply = () => {
+  refetch()
+  };
 
   return (
     <div className="gigs">
@@ -32,9 +47,9 @@ export const Gigs = () => {
         <div className="menu">
           <div className="left">
             <span>Budget</span>
-            <input type="text" placeholder="min" />
-            <input type="text" placeholder="max" />
-            <button>Apply</button>
+            <input type="text" ref={minRef} placeholder="min" />
+            <input type="text" ref={maxRef} placeholder="max" />
+            <button onClick={apply}>Apply</button>
           </div>
           <div className="right">
             <span className="sortBy">SortBy</span>
@@ -58,9 +73,11 @@ export const Gigs = () => {
           </div>
         </div>
         <div className="cards">
-          {gigs.map((x) => (
-            <GigCard key={x.id} item={x} />
-          ))}
+          {isLoading
+            ? "loading"
+            : error
+            ? "Something went wrong"
+            : data.map((x) => <GigCard key={x._id} item={x} />)}
         </div>
       </div>
     </div>
