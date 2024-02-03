84787c960dd0bed7fa24cc844a4c9850f6814a8a
Maja Petkova <majapetkova1@yahoo.com>
2024-02-03 11:46:59 +0100

    Update Message template

--- fiverr-app/src/pages/message/Message.jsx
+++ fiverr-app/src/pages/message/Message.jsx
@@ -1,79 +1,72 @@
 import React from "react";
 import "./message.scss";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 export const Message = () => {
+  const { id } = useParams();
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["messages"],
+    queryFn: () =>
+      newRequest.get(`/messages/${id}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (message) => {
+      return newRequest.post(`/messages`, message);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["messages"]);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutation.mutate({
+      conversationId: id,
+      desc: e.target[0].value,
+    });
+    e.target[0].value = "";
+  };
+
   return (
     <div className="message">
       <div className="container">
         <span className="breadCrumbs">
-          <Link to="/messages">MESSAGE </Link>>JOHN DOE>
+          <Link to="/messages">MESSAGE</Link> > John Doe >
         </span>
+        {isLoading ? 
+          "Loading..."
+         : error ? 
+          "error"
+         : (
         <div className="messages">
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/99562/texture-handwriting-sutterlin-vintage-99562.jpeg?auto=compress&cs=tinysrgb&w=400"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-              modi incidunt corporis quisquam rerum voluptatum culpa, ad minus
-              ipsam perspiciatis repudiandae consectetur adipisci laborum,
-              fugiat amet ducimus minima. Magni aperiam libero dolorum dolor
-              sunt vero corporis pariatur consequatur excepturi nobis.
-            </p>
-          </div>
-          <div className="item owner">
-            <img
-              src="https://images.pexels.com/photos/99562/texture-handwriting-sutterlin-vintage-99562.jpeg?auto=compress&cs=tinysrgb&w=400"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-              modi incidunt corporis quisquam rerum voluptatum culpa, ad minus
-              ipsam perspiciatis repudiandae consectetur adipisci laborum,
-              fugiat amet ducimus minima. Magni aperiam libero dolorum dolor
-              sunt vero corporis pariatur consequatur excepturi nobis.
-            </p>
-          </div>
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/99562/texture-handwriting-sutterlin-vintage-99562.jpeg?auto=compress&cs=tinysrgb&w=400"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-              modi incidunt corporis quisquam rerum voluptatum culpa, ad minus
-              ipsam perspiciatis repudiandae consectetur adipisci laborum,
-              fugiat amet ducimus minima. Magni aperiam libero dolorum dolor
-              sunt vero corporis pariatur consequatur excepturi nobis.
-            </p>
-          </div>
-          <div className="item owner">
-            <img
-              src="https://images.pexels.com/photos/99562/texture-handwriting-sutterlin-vintage-99562.jpeg?auto=compress&cs=tinysrgb&w=400"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-              modi incidunt corporis quisquam rerum voluptatum culpa, ad minus
-              ipsam perspiciatis repudiandae consectetur adipisci laborum,
-              fugiat amet ducimus minima. Magni aperiam libero dolorum dolor
-              sunt vero corporis pariatur consequatur excepturi nobis.
-            </p>
-          </div>
+          {data.map((m) => (
+            <div
+              className={m.userId === currentUser._id ? "owner item" : "item"}
+              key={m._id}
+            >
+              <img
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <p>{m.desc}</p>
+            </div>
+          ))}
         </div>
+         )} 
         <hr />
-        <div className="write">
-          <textarea
-            name=""
-            placeholder="Write a message..."
-            id=""
-            cols="30"
-            rows="10"
-          ></textarea>
-          <button>Send</button>
-        </div>
+        <form className="write" onSubmit={handleSubmit}>
+          <textarea type="text" placeholder="write a message" />
+          <button type="submit">Send</button>
+        </form>
       </div>
     </div>
   );
