a299c856232d2e6904e4a01d7a930b7b2f8b05e8
Maja Petkova <majapetkova1@yahoo.com>
2024-02-22 13:43:34 +0100

    Update css and implement useReducer to Add page

--- fiverr-app/src/pages/add/Add.jsx
+++ fiverr-app/src/pages/add/Add.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "./add.scss";
+import { INITIAL_STATE, gigReducer } from "../../reducers/gigReducer";
+import upload from "../../utils/upload";
 
 export const Add = () => {
+  const [singleFile, setSingleFile] = useState(undefined);
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+
+  const [state, dispatch] = useReducer(gigReducer, INITIAL_STATE);
+
+  const handleChange = (e) => {
+    dispatch({
+      type: "CHANGE_INPUT",
+      payload: { name: e.target.name, value: e.target.value },
+    });
+  };
+  const handleFeature = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_FEATURE",
+      payload: e.target[0].value,
+    });
+    e.target[0].value = "";
+  };
+  const handleUploads = async (e) => {
+    setUploading(true);
+    try {
+      const cover = await upload(singleFile);
+      const images = await Promise.all(
+        [...files].map(async (x) => {
+          const url = await upload(x);
+          return url;
+        })
+      );
+      setUploading(false);
+      dispatch({ type: "ADD_IMAGES", payload: { cover, images } });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log([...files]);
   return (
     <div className="add">
       <div className="container">
@@ -11,51 +50,89 @@ export const Add = () => {
             <label htmlFor="">Title</label>
             <input
               type="text"
+              name="title"
               placeholder="e.g. I will do something I'm really good at"
+              onChange={handleChange}
             />
             <label htmlFor="">Category</label>
-            <select name="categories" id="categories">
+            <select name="cat" id="cat" onChange={handleChange}>
               <option value="design">Design</option>
               <option value="web">Web Development</option>
               <option value="animation">Animation</option>
               <option value="music">Music</option>
             </select>
-            <label htmlFor="">Cover Image</label>
-            <input type="file" />
-            <label htmlFor="">Upload Images</label>
-            <input type="file" multiple />
+            <div className="images">
+              <div className="imagesInputs">
+                <label htmlFor="">Cover Image</label>
+                <input
+                  type="file"
+                  onChange={(e) => setSingleFile(e.target.files[0])}
+                />
+                <label htmlFor="">Upload Images</label>
+                <input
+                  type="file"
+                  multiple
+                  onChange={(e) => setFiles(e.target.files)}
+                />
+              </div>
+              <button className="btn">{uploading ? "Uploading" : "Upload"  }</button>
+            </div>
             <label htmlFor="">Description</label>
             <textarea
-              name=""
+              name="desc"
               id=""
               cols="30"
               rows="10"
               placeholder="Brief descriptions to introduce your service to customers"
+              onChange={handleChange}
             ></textarea>
-            <button>Create</button>
+            <button className="btn">Create</button>
           </div>
           <div className="right">
             <label htmlFor="">Service Title</label>
-            <input type="text" placeholder="e.g. One-page web design" />
+            <input
+              type="text"
+              name="shortTitle"
+              placeholder="e.g. One-page web design"
+              onChange={handleChange}
+            />
             <label htmlFor="">Short Description</label>
             <textarea
-              name=""
+              onChange={handleChange}
+              name="shortDesc"
               id=""
               cols="30"
               rows="10"
               placeholder="Short description of your service"
             ></textarea>
             <label htmlFor="">Delivery Time (e.g. 3 days)</label>
-            <input type="number" min={1} />
+            <input
+              type="number"
+              min={1}
+              name="deliveryTime"
+              onChange={handleChange}
+            />
             <label htmlFor="">Revision Number</label>
-            <input type="number" min={1} />
+            <input
+              type="number"
+              min={1}
+              name="revisionNumber"
+              onChange={handleChange}
+            />
             <label htmlFor="">Add Features</label>
-            <input type="text" placeholder="e.g page design" />
-            <input type="text" placeholder="e.g. file uploading" />
-            <input type="text" placeholder="e.g. setting up a domain" />
-            <input type="text" placeholder="e.g hosting" />
+            <form action="" className="add" onSubmit={handleFeature}>
+              <input type="text" placeholder="e.g page design" />
+              <button type="submit" className="btn">
+                Add
+              </button>
+            </form>
+            <div className="addedFeatures">
+              <div className="item">
+               <button className="btn">feature <span>X</span></button> 
+              </div>
+            </div>
             <label htmlFor="">Price</label>
-            <input type="number" min={1} />
+            <input type="number" min={1} name="price" onChange={handleChange} />
           </div>
         </div>
       </div>
