7b17b3cf8825665c7c9088e86786dc2274755e00
Maja Petkova <majapetkova1@yahoo.com>
2024-05-23 22:42:23 +0200

    Add html and style for todo component

--- todo2/vite-project/src/components/Todo.jsx
+++ todo2/vite-project/src/components/Todo.jsx
@@ -1,11 +1,16 @@
 import "./CSS/Todo.css";
 
-const Todo =()=> {
+const Todo = () => {
   return (
     <div className="todo">
-
+      <div className="todo-header">To-Do List</div>
+      <div className="todo-add">
+        <input type="text" placeholder="Add your task" className="todo-input" />
+        <div className="todo-add-btn">ADD</div>
+      </div>
+      <div className="todo-list"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
