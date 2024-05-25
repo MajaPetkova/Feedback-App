617bf14090fe38c725e03914b558d88a82859d85
Maja Petkova <majapetkova1@yahoo.com>
2024-05-25 11:23:11 +0200

    Add assets, add html and style for todoItems and update todo.jsx

--- todo2/vite-project/src/components/Todo.jsx
+++ todo2/vite-project/src/components/Todo.jsx
@@ -1,14 +1,58 @@
+import { useRef, useState } from "react";
 import "./CSS/Todo.css";
+import TodoItems from "./TodoItems";
+
+let count = 0;
 
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  const add = () => {
+    setTodos([
+      ...todos,
+      {
+        no: count++,
+        text: inputRef.current.value,
+        display: "",
+      },
+    ]);
+    inputRef.current.value = "";
+  };
+
+  useState(() => {}, [todos]);
+
   return (
     <div className="todo">
       <div className="todo-header">To-Do List</div>
       <div className="todo-add">
-        <input type="text" placeholder="Add your task" className="todo-input" />
-        <div className="todo-add-btn">ADD</div>
+        <input
+          type="text"
+          placeholder="Add your task"
+          className="todo-input"
+          ref={inputRef}
+        />
+        <div
+          className="todo-add-btn"
+          onClick={() => {
+            add();
+          }}
+        >
+          ADD
+        </div>
       </div>
-      <div className="todo-list"></div>
+      <div className="todo-list">
+        {todos.map((x, index) => {
+          return (
+            <TodoItems
+              key={index}
+              no={x.no}
+              display={x.display}
+              text={x.text}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- todo2/vite-project/src/components/TodoItems.jsx
+++ todo2/vite-project/src/components/TodoItems.jsx
@@ -1,7 +1,20 @@
-import "./CSS/TodoItems.css"
- const TodoItems = () => {
+import "./CSS/TodoItems.css";
+import tick from "./Assets/tick.png";
+import notTick from "./Assets/not_tick.png";
+import cross from "./Assets/cross.png";
+
+const TodoItems = ({no, text, display }) => {
   return (
-    <div>TodoItems</div>
-  )
-}
-export default TodoItems
+    <div className="todo-item">
+      <div className="todoItems-container">
+        <img src={notTick} alt="" />
+        <img src={tick} alt="" />
+        <div className="todoItems-text">
+           {text}
+        </div>
+      </div>
+        <img src={cross} alt="" className="todo-icon" />
+    </div>
+  );
+};
+export default TodoItems;
