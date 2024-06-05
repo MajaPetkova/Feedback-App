426f8ba6508e6d2d586688fc7c21f25a9b4f84af
Maja Petkova <majapetkova1@yahoo.com>
2024-06-05 20:12:28 +0200

    Add navbar component and style for the navbar

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./navbar.css"
+import logo from "../../assets/logo.webp"
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        
+        <img src={logo} alt="" className='logo-img' />
+        <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <div className="nav-connect">Connect with Me</div>
     </div>
   )
 }
