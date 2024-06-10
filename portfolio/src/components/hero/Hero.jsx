d7c6d968e18d533149e2af203320ffbe133562db
Maja Petkova <majapetkova1@yahoo.com>
2024-06-10 19:31:52 +0200

    Add smooth scroll on the website

--- portfolio/src/components/hero/Hero.jsx
+++ portfolio/src/components/hero/Hero.jsx
@@ -1,18 +1,25 @@
 import "./hero.css";
-import profile from "../../assets/ProfilePic.jpg"
+import profile from "../../assets/ProfilePic.jpg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero" id="home">
       <img src={profile} alt="" className="hero-profile" />
-      <h1><span>I am Maya Petkova, </span>JavaScript developer based in Berlin</h1>
+      <h1>
+        <span>I am Maya Petkova, </span>JavaScript developer based in Berlin
+      </h1>
       <p>
         Motivated, enthusiastic and creative person looking to pursue a
         successful career in software development, where I can help in the
         delivery of modern software solutions.
       </p>
       <div className="hero-actions">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me{" "}
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume </div>
       </div>
     </div>
--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,23 +1,46 @@
-
-import "./navbar.css"
-import logo from "../../assets/logo.webp"
-import theme_pattern from "../../assets/theme_pattern.svg"
-
+import "./navbar.css";
+import logo from "../../assets/logo.webp";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import underline from "../../assets/nav_underline.svg";
+import { useState } from "react";
+import  AnchorLink  from "react-anchor-link-smooth-scroll";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <img src={logo} alt="" className='logo-img' />
-        <img src={theme_pattern} alt="" className='theme'/>
-        <ul className='nav-menu'>
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-        </ul>
-        <div className="nav-connect">Connect with Me</div>
-    </div>
-  )
-}
+  const [menu, setMenu] = useState("home");
 
-export default Navbar
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" className="logo-img" />
+      <img src={theme_pattern} alt="" className="theme" />
+      <ul className="nav-menu">
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#home">
+            <p onClick={() => setMenu("home")}>Home</p>
+          </AnchorLink >
+          {menu === "home" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+        <AnchorLink className="anchor-link"  offset={50} href="#about">
+          <p onClick={() => setMenu("about")}>About Me</p>
+          {menu === "about" ? <img src={underline} alt="" /> : <></>}
+          </AnchorLink>
+        </li>
+        <li>
+        <AnchorLink className="anchor-link" offset={50} href="#work">
+          <p onClick={() => setMenu("work")}>Portfolio</p>
+          {menu === "work" ? <img src={underline} alt="" /> : <></>}
+          </AnchorLink>
+        </li>
+        <li>
+        <AnchorLink className="anchor-link" offset={50} href="#contact">
+          <p onClick={() => setMenu("contact")}>Contact</p>
+          {menu === "contact" ? <img src={underline} alt="" /> : <></>}
+          </AnchorLink>
+        </li>
+      </ul>
+      <div className="nav-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">Connect with Me</AnchorLink></div>
+    </div>
+  );
+};
+
+export default Navbar;
