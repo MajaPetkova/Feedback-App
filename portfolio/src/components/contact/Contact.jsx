f6a72ac9efb30b592e23e16aaf2c6471cc45a289
Maja Petkova <majapetkova1@yahoo.com>
2024-06-10 19:53:59 +0200

    Add sending message functionality

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -3,8 +3,34 @@ import theme_pattern from "../../assets/theme_pattern.svg";
 import mail_icon from "../../assets/mail_icon.svg";
 import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
+import { useState } from "react";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "e834ea8f-ab8b-4701-8847-9ac65969591b");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+      alert("Your message has been sent successfully")
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className="contact" id="contact">
       <div className="contact-title">
@@ -32,7 +58,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <form action="" className="contact-right">
+        <form action="" className="contact-right" onSubmit={onSubmit}>
           <label htmlFor="">Your Name</label>
           <input type="text" placeholder="Enter your name" name="name" />
           <label htmlFor="">Your Email</label>
